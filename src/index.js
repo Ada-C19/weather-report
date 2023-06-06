3370735778d2c892def5af2fff7900678f9050cc
Limary Gonzalez <limaryg11@gmail.com>
2023-06-06 15:17:20 -0700

    adding logic for changing color of up and down button

--- src/index.js
+++ src/index.js
@@ -6,11 +6,31 @@ const state = {
   temp: 72,
 };
 
-// incrase
+// changing font color
+const changeTempFontColor = () => {
+    let temp = state.temp;
+    if (temp >= 80) {
+        color = 'red';
+    }else if (temp >= 70) {
+        color = 'orange';
+    }else if (temp >= 60) {
+        color = 'yellow';
+    }else if (temp >= 50) {
+        color = 'green';
+    }else {
+        color = 'brown';
+    }
+
+};
+
+
+
+// increase
 const increaseTemp = () => {
     state.temp += 1;
     const upTempNumber = document.getElementById("tempnumber");
     upTempNumber.textContent = `${state.temp}`;
+    changeTempFontColor();
 
 }
 const increaseButton = document.getElementById("up");
@@ -21,6 +41,7 @@ const decreaseTemp = () => {
   state.temp -= 1;
   const downTempNumber = document.getElementById("tempnumber");
   downTempNumber.textContent = `${state.temp}`;
+  changeTempFontColor();
 };
 
 const decreaseButton = document.getElementById("down");
