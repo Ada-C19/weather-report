656c0840c03ed47b60845ac4c8983405b9cc12df
Limary Gonzalez <limaryg11@gmail.com>
2023-06-06 14:44:21 -0700

    adding changes for wave 2

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import 'regenerator-runtime/runtime';
+import axios from 'axios';
+
+const state = {
+  city: 'Seattle',
+  temp: 72,
+};
+
+const increaseTemp = () => {
+    let up = document.getElementById('up')
+}
+
