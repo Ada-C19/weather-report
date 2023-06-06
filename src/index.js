819779660f71ef6aff1017270ccda0980bd5c7b6
Kira Sidhu <kira.kaur.sidhu@gmail.com>
2023-06-06 14:33:17 -0700

    Started temp increment in index.js for wave 2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const state = {
+    degrees: 62
+};
+
+const addDegreen = (event) => {
+    const newDegree = document.createElement("span");
+}
+
+const button1 = document.getElementById("up");
+button1.addEventListener("click", () => {
+    document.body.className = "degrees";
+
+})
