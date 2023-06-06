5c83917be934b2fb61bac2c43207d141c49f4358
Ana Silvera <anasilverx@gmail.com>
2023-06-06 14:43:17 -0700

    implemented functionality for up and down arrows to increase temp

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 "use strict";
+
+const state = {
+    temperature: 0
+};
+
+const increaseTemp = (event) => {
+    state.temperature += 1;
+    const tempCount = document.querySelector("#temp-count");
+    tempCount.textContent = `${state.temperature}`;  
+};
+
+const decreaseTemp = (event) => {
+    state.temperature -= 1;
+    const tempCount = document.querySelector("#temp-count");
+    tempCount.textContent = `${state.temperature}`;  
+};
+
+
+const registerEventHandlers = (event) => {
+    const tempUpButton = document.querySelector("#up-arrow");
+    tempUpButton.addEventListener("click", increaseTemp);
+
+    const tempDownButton = document.querySelector("#down-arrow");
+    tempDownButton.addEventListener("click", decreaseTemp);
+};
+document.addEventListener("DOMContentLoaded", registerEventHandlers)
