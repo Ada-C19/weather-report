918e3460cfeda8abfa9fe5ae511bf8ea2f5a7673
Jessie <wajliu3@gmail.com>
2023-06-06 14:55:38 -0700

    add header and temp flex

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const state = {
+    temp:69,
+}
+
+const updateTemp = () => {
+    const 
+}
+state.temp += 1
+const changeTemp = document.querySelector("#temp")
+temp.textContent = `{${state.temp}}`
