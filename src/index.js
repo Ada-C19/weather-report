238c2939df05c1caf72fc73d223e9c06397075e0
Dee Williams <deejmwilliams@gmail.com>
2023-06-07 16:22:42 -0400

    Update header city to change via text input

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
+const cityUpdate = () => {
+  const newCityText = state.cityTextField.value;
+  state.headerCity.textContent = newCityText;
+};
+
 const loadData = () => {
-  //Use this to assign HTML elements to variables
+  state.cityTextField = document.querySelector("#cityNameInput");
+  state.headerCity = document.querySelector("#headerCityName");
 };
 
 const registerEventHandlers = (event) => {
   //All event handlers will be called here
   loadData();
+  state.cityTextField.addEventListener("input", cityUpdate);
 };
 
 const state = {
-  //Add state variables here
+  cityTextField: null,
+  headerCity: null,
 };
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
