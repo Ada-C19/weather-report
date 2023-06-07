85bfef6f08ecfc3411d591fcb201c329fce68906
Carolyn Qi <qi.carolyn@gmail.com>
2023-06-06 22:42:28 -0700

    Add landscape values and tie to the current temperature

--- src/index.js
+++ src/index.js
@@ -17,20 +17,42 @@ const getTemp = () => {
         temperature.className = "cold"
     }
 }
+
 const button1 = document.getElementById("increase_temperature");
 button1.addEventListener("click", () => {
     current_temp = current_temp + 1;
     getTemp();
+    getLandscape();
 });
 
 const button2 = document.getElementById("decrease_temperature");
 button2.addEventListener("click", () => {
     current_temp = current_temp - 1;
     getTemp();
+    getLandscape();
 });
+
+const getLandscape = () => {
+    const landscape = document.getElementById("landscape");
+    if (current_temp >= 80) {
+        landscape.textContent = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂"
+    } else if (current_temp >= 70) {
+        landscape.textContent = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷"
+    } else if (current_temp >= 60) {
+        landscape.textContent = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃"
+    } else if (current_temp >= 50) {
+        landscape.textContent = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲"
+    } else {
+        landscape.textContent = "⛄️⛄️🎁❄❄⛄️🦌❄🎁⛄️⛄️🦌🦌⛄️🎁"
+    }
+}
 
 if (document.readyState !== "loading") {
     getTemp();
+    getLandscape();
 } else {
     document.addEventListener("DOMContentLoaded", getTemp);
+    document.addEventListener("DOMContentLoaded", getLandscape);
 }
+
+
