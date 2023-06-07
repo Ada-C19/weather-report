bd8d6f7b8bb8cd363e9a3706906abe72a699e661
Jiaping Chen <jiapingchen17@gmail.com>
2023-06-06 17:27:35 -0700

    made the temp increase and decrease work

--- src/index.js
+++ src/index.js
@@ -7,20 +7,27 @@ const increaseTemp = () => {
     updateTemp();
 };
 
+// this is new
+const decreaseTemp = () => {
+    state.temp -= 1;
+    updateTemp();
+}
+
 const updateTemp = () => {
     let temp = state.temp;
     let color = "above80";
 
     if (state.temp >= 80) {
-        let color = "above80";
-    } else if (70 <= state.temp < 80) {
-        let color = "seventies";
-    } else if (60 <= state.temp < 70) {
-        let color = "sixties";
-    } else if (50 <= state.temp < 60) {
-        let color = "fifties";
+        color = "above80";
+    // some changes here to add the "&& state.temp"
+    } else if (70 <= state.temp && state.temp < 80) {
+        color = "seventies";
+    } else if (60 <= state.temp && state.temp < 70) {
+        color = "sixties";
+    } else if (50 <= state.temp && state.temp < 60) {
+        color = "fifties";
     } else {
-        let color = "below49";
+        color = "below49";
     }
 
     const temperature = document.getElementById('temp_value');
@@ -29,9 +36,14 @@ const updateTemp = () => {
 }
 
 const registerEventHandlers = () => {
-    updateTemp()l
+    updateTemp();
     const increaseButton = document.getElementById("increase_temp");
     increaseButton.addEventListener("click", increaseTemp);
+
+    // this is new
+    const decreaseButton = document.getElementById("decrease_temp");
+    decreaseButton.addEventListener("click", decreaseTemp);
 }
+
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
