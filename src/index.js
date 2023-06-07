7a92b9f088f7847914841d2ba146f8c195f78579
Dee Williams <deejmwilliams@gmail.com>
2023-06-07 18:08:35 -0400

    Add emoji to landscape

--- src/index.js
+++ src/index.js
@@ -8,25 +8,56 @@ const cityReset = () => {
   state.headerCity.textContent = state.cityDefault;
 };
 
+const landscapeUpdate = () => {
+  let newLandscape;
+  if (state.temperature >= 80) {
+    newLandscape = landscapes.hotLandscape;
+  } else if (state.temperature >= 70) {
+    newLandscape = landscapes.warmLandscape;
+  } else if (state.temperature >= 60) {
+    newLandscape = landscapes.coolLandscape;
+  } else {
+    newLandscape = landscapes.coldLandscape;
+  }
+
+  state.landscapeField.textContent = newLandscape;
+};
+
 const loadData = () => {
   //Assign selected HTML elements to attributes of state
   state.cityTextField = document.querySelector("#cityNameInput");
   state.headerCity = document.querySelector("#headerCityName");
   state.cityResetButton = document.querySelector("#cityNameReset");
+  state.landscapeField = document.querySelector("#landscape");
+  state.temperature = document.querySelector("#tempValue").textContent;
+  state.currTempButton = document.querySelector("#currentTempButton");
 };
 
 const registerEventHandlers = (event) => {
   //All event handlers will be defined here
   loadData();
+  landscapeUpdate();
   state.cityTextField.addEventListener("input", cityUpdate);
   state.cityResetButton.addEventListener("click", cityReset);
+  state.currTempButton.addEventListener("click", landscapeUpdate);
 };
 
 const state = {
   cityTextField: null,
   headerCity: null,
-  cityDefault: "Boston",
   cityResetButton: null,
+  landscapeField: null,
+  temperature: null,
+  currTempButton: null,
 };
+
+const landscapes = {
+  coldLandscape: "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲",
+  coolLandscape: "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃",
+  warmLandscape: "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷",
+  hotLandscape: "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂",
+};
+
+const cityDefault = "Boston";
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
