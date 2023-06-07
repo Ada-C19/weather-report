8486662300c8c27648217db6d2897842ed43bdbf
TombAnneX <109308405+AKTuminello@users.noreply.github.com>
2023-06-07 09:34:18 -0600

    variables assigned to DOM elements, basic css

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const increaseButton=document.getElementById('increase-button');
+const decreaseButton=document.getElementById('decrease-button');
+const temperature=document.getElementById('temperature');
+const cityNameElement=document.getElementById('city-name');
+const cityInput=document.getElementById('city-input');
+const resetButton=document.getElementById('reset-button');
+const skySelect=document.getElementById('sky-select');
+const skyElement=document.getElementById('sky');
+const fetchButton=document.getElementById('fetch-button');
+// add fetch button to css
+
+/* K to F/c conversion function
+probably need two for C and F 
+
+function kelvinToCelcius(kelvin) {
+    return Math.round (kelvin - 273.15);
+}
+K to F
+
+function kelvinToFahrenheit(kelvin) {
+    return Math.round((kelvin-273.15)*9/5+32);
+    
+]*/
