c0fb72ff47341b210223e0adece40c07a6ffc3a4
Carolyn Qi <qi.carolyn@gmail.com>
2023-06-06 22:28:56 -0700

    Add temperature and change the color as it goes into different ranges. And make sure buttons change the temperature value

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 "use strict";
+
+let current_temp = 72
+
+const getTemp = () => {
+    const temperature = document.getElementById("temp");
+    temperature.textContent = current_temp;
+    if (current_temp >= 80) {
+        temperature.className = "hot"
+    } else if (current_temp >= 70) {
+        temperature.className = "warm"
+    } else if (current_temp >= 60) {
+        temperature.className = "medium"
+    } else if (current_temp >= 50) {
+        temperature.className = "cool"
+    } else {
+        temperature.className = "cold"
+    }
+}
+const button1 = document.getElementById("increase_temperature");
+button1.addEventListener("click", () => {
+    current_temp = current_temp + 1;
+    getTemp();
+});
+
+const button2 = document.getElementById("decrease_temperature");
+button2.addEventListener("click", () => {
+    current_temp = current_temp - 1;
+    getTemp();
+});
+
+if (document.readyState !== "loading") {
+    getTemp();
+} else {
+    document.addEventListener("DOMContentLoaded", getTemp);
+}
