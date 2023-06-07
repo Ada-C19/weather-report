40d212c6e230714d447d8402ca0dad1b43de4367
Say Ryder <lindz.ryder@gmail.com>
2023-06-07 16:53:48 -0400

    start wave 2

---
+++ src/index.js
@@ -0,0 +1,12 @@
+"use strict";
+const tempValue = document.getElementById('tempValue');
+const increaseTempControl = document.getElementById('increaseTempControl');
+const decreaseTempControl = document.getElementById('decreaseTempControl');
+const skySelector = document.getElementById('skySelector');
+const gardenContent = document.getElementById('gardenContent');
+let temperature = 60;
+tempCheck = (temperature) => {
+    if (temperature >= 80) {
+        
+    }
+}
