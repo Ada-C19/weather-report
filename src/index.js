c83d83263c7e271c59c509508cc137659c3bed5c
Ana Villalobos <ana.villalobos309@gmail.com>
2023-06-06 18:17:08 -0700

    Clickable button to inc/dec temp

--- src/index.js
+++ src/index.js
@@ -4,12 +4,43 @@ const state = {
     degrees: 62
 };
 
-const addDegreen = (event) => {
-    const newDegree = document.createElement("span");
-}
+// const addDegreen = (event) => {
+//     const newDegree = document.createElement("span");
+// }
 
-const button1 = document.getElementById("up");
-button1.addEventListener("click", () => {
-    document.body.className = "degrees";
+// const button1 = document.getElementById("up");
+// button1.addEventListener("click", () => {
+//     document.body.className = "degrees";
 
-})
+// })
+
+// const up = document.getElementById("up");
+// up.addEventListener("click", () => {
+//     const tempNum = document.getElementsByClassName('degrees');
+//     tempNum.textContent = state.degrees += 1;
+
+// })
+
+
+
+const increaseTemp = (event) => {
+    state.degrees += 1;
+    const degreeCount = document.querySelector('#degrees');
+    degreeCount.textContent = state.degrees;
+};
+
+const decreaseTemp = (event) => {
+    state.degrees -= 1;
+    const degreeCount = document.querySelector('#degrees');
+    degreeCount.textContent = state.degrees;
+};
+
+const registerEventHandlers = (event) => {
+    const upButton = document.querySelector('#up');
+    upButton.addEventListener("click", increaseTemp);
+
+    const downButton = document.querySelector('#down');
+    downButton.addEventListener("click", decreaseTemp);
+};
+
+document.addEventListener("DOMContentLoaded", registerEventHandlers);
