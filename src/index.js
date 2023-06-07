f476732a940a915788e8f0f8c20c1910f5eb6fb4
Anh Huynh <avh2@uw.edu>
2023-06-06 17:18:26 -0700

    Modified index.html and added pseudocode to index.js files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Wave 2 pseudocode:
+// - Button to change temp
+// - Up arrow button
+// - Down arrow button
+// - Clicking temp button changes:
+//   + color of number
+//   + color of background
+//   + landscape image
