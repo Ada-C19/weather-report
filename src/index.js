7f9391564d50599f8d4debe1b9a788f28f0a9f04
Trang Tran <trantrang181197@gmail.com>
2023-06-07 16:36:30 -0700

    color changing not working

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ increaseButton.addEventListener("click", () => {
     const tempValue = document.getElementById("tempValue");
     tempValueInt++;
     tempValue.innerHTML = tempValueInt;
+    tempValue.className = tempValueInt < 49 ? "teal"
+        : tempValueInt >= 50 && tempValueInt <= 59 ? "green"
+        : tempValueInt >= 60 && tempValueInt <= 69 ? "yellow"
+        : tempValueInt >= 70 && tempValueInt <= 79 ? "orange"
+        : "red";
+    // if (tempValueInt < 49) {
+    //     tempValue.className = "blue";
+    // } else if (tempValueInt >= 50 && tempValueInt <= 59) {
+    //     tempValue.className = "green";
+    // }
 });
 
 const decreaseButton = document.getElementById("bottomButton");
