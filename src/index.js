f5989bd0f02d998d3f41e21bfaa0a231b49262c2
Dee Williams <deejmwilliams@gmail.com>
2023-06-07 16:31:46 -0400

    Wave 2 complete

--- src/index.js
+++ src/index.js
@@ -3,20 +3,32 @@ const cityUpdate = () => {
   state.headerCity.textContent = newCityText;
 };
 
+const cityReset = () => {
+  state.cityTextField.value = state.cityDefault;
+  state.headerCity.textContent = state.cityDefault;
+};
+
 const loadData = () => {
+  //Assign selected HTML elements to attributes of state
+  //Initialize value of header city
   state.cityTextField = document.querySelector("#cityNameInput");
   state.headerCity = document.querySelector("#headerCityName");
+  state.headerCity.textContent = state.cityTextField.value;
+  state.cityResetButton = document.querySelector("#cityNameReset");
 };
 
 const registerEventHandlers = (event) => {
   //All event handlers will be called here
   loadData();
   state.cityTextField.addEventListener("input", cityUpdate);
+  state.cityResetButton.addEventListener("click", cityReset);
 };
 
 const state = {
   cityTextField: null,
   headerCity: null,
+  cityDefault: "Boston",
+  cityResetButton: null,
 };
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
