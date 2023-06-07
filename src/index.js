fc09353495179a76a3979557b6c645d9b03a3dfc
nadia <reach.nadia.saria@gmail.com>
2023-06-07 14:30:44 -0700

    add setTempColor function

--- src/index.js
+++ src/index.js
@@ -14,20 +14,29 @@ const loadControls = () => {
 const handleIncreaseTempClicked = () => {
     ++state.temperature;
     state.tempValue.textContent = state.temperature;
+    setTempColor();
 };
 
 const handleDecreaseTempClicked = () => {
     --state.temperature;
     state.tempValue.textContent = state.temperature;
+    setTempColor();
 };
 
-
+const setTempColor = () => {
+    let colorIdentifier = Math.floor(state.temperature / 10) 
+    state.tempValue.classList.toggle('teal', colorIdentifier <= 4);
+    state.tempValue.classList.toggle('green', colorIdentifier === 5);
+    state.tempValue.classList.toggle('yellow', colorIdentifier === 6);
+    state.tempValue.classList.toggle('orange', colorIdentifier === 7);
+    state.tempValue.classList.toggle('red', colorIdentifier >= 8);
+}
 
 const registerEventHandlers = () => {
     loadControls();
+    setTempColor();
     state.increaseTempButton.addEventListener("click", handleIncreaseTempClicked);
     state.decreaseTempButton.addEventListener("click", handleDecreaseTempClicked);
 };
-
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
