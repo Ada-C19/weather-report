d1086b7f9141490b9783668ade982b2edb0b3bd3
halimAremu <halimat3000@gmail.com>
2023-06-07 16:21:49 -0500

    wave 1 done

--- src/index.js
+++ src/index.js
@@ -3,3 +3,50 @@ const state = {
     temp: 0,
     landscape:"🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲"
 }
+
+const temp = document.getElementById("tempValue")
+const controlUp = document.getElementById("increaseTempControl")
+const controlDown = document.getElementById("decreaseTempControl")
+const landscape = document.getElementById("landscape")
+
+
+const changeColor = () =>{
+    if (state.temp>80){
+        temp.className = 'red';
+        landscape.innerText = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂";
+    }
+    else if (state.temp>69){
+        temp.className = 'orange';
+        landscape.innerText = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷"
+    }
+    else if(state.temp>59){
+        temp.className = 'yellow';
+        landscape.innerText = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃"
+    }
+    else if(state.temp>49){
+        temp.className = 'green'
+        landscape.innerText = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲"
+    }
+    else{
+        temp.className = 'teal'
+        landscape.innerText = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲"
+    }
+}
+
+const increaseTemp = () =>{
+    changeColor()
+    state.temp+= 1
+    temp.innerText = state.temp
+    
+}
+
+controlUp.addEventListener('click', increaseTemp)
+
+const decreaseTemp = () => {
+    changeColor()
+    state.temp -= 1
+    temp.innerText = state.temp
+    
+}
+
+controlDown.addEventListener('click', decreaseTemp)
