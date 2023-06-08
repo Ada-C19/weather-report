c7175814114410b1b5cc2f3f2f3e2b39314b613e
Sarah Kim <kimsuyon2013@gmail.com>
2023-06-08 13:24:32 -0700

    added color change to temp and land

--- src/index.js
+++ src/index.js
@@ -1,50 +1,69 @@
-const tempState = {
+const state = {
     liveTemp : 68
 };
 
+const changeTempAndLand = () => {
+    let temp = state.liveTemp;
+    let color = 'yellow';
+    let land = "🌾🌾_🍃_🪨_🛤_🌾🌾_🍃";
+    if (temp >= 80) {
+        color = 'hot';
+        land = "🌵_🐍🌵🦂_🌵🌵__🐍🏜🦂";
+    } else if (temp >= 70) {
+        color = 'warm';
+        land = "🌸🌿🌼🌱_🌷🌻🌿_🌱🌻🌷";
+    } else if (temp >= 60) {
+        color = 'moderate';
+        land = "🌾🌾_🍃_🪨_🛤_🌾🌾_🍃";
+    } else if (temp >= 50) {
+        color = 'cool';
+        land = "🌲⛄️🌲⛄️🍂🌲🍁🌲⛄️🍂🌲";
+    } else {
+        color = 'cold';
+        land = "🌲⛄️🌲⛄️🍂🌲🍁🌲⛄️🍂🌲";
+    }
+    const temperature = document.getElementById('live_temp');
+    temperature.textContent = state.liveTemp;
+    temperature.className = color;
+    const landscape = document.getElementById('garden_land');
+    landscape.textContent = land;
+};
 
-let temp = tempState.liveTemp;
-let color = 'yellow';
-if (temp >= 80) {
-    color = 'hot';
-} else if (temp >= 70) {
-    color = 'warm';
-} else if (temp >= 60) {
-    color = 'moderate';
-} else if (temp >= 50) {
-    color = 'cool';
-} else {
-    color = 'cold';
-}
 
-const temperature = document.getElementById('live_temp');
-temperature.className = color;
-temperature.textContent = String(tempState.liveTemp);
-
-const tempIncrease = (event) => {
-    
+const tempIncrease = () => {
     //temp behavior
-    tempState.liveTemp += 1;
-    const liveTempContainer = document.querySelector("#live_temp");
-    liveTempContainer.textContent = `${tempState.liveTemp}`;
+    state.liveTemp += 1;
+};
 
-}
-const tempDecrease = (event) => {
-    
+const tempDecrease = () => {
     //temp behavior
-    tempState.liveTemp -= 1;
-    const liveTempContainer = document.querySelector("#live_temp");
-    liveTempContainer.textContent = `${tempState.liveTemp}`;
+    state.liveTemp -= 1;
+};
 
-}
 
-const registerEventHandlers = (event) => {
+
+
+
+
+
+const registerEventHandlers = () => {
     const tempIncButton = document.querySelector("#increase_button");
-    tempIncButton.addEventListener("click", tempIncrease);
+    tempIncButton.addEventListener('click', tempIncrease);
+    tempIncButton.addEventListener('click', () =>{
+        changeTempAndLand()
+    });
+
     const tempDecButton = document.querySelector("#decrease_button");
-    tempDecButton.addEventListener("click", tempDecrease);
-}
+    tempDecButton.addEventListener('click', tempDecrease);
+    tempDecButton.addEventListener('click', () => {
+        changeTempAndLand()
+    })
+};
+
+
+
 
 
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
+
