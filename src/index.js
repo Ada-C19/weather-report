7fd4fc308ebfd890be62c93000321d5e5454332c
Ana Silvera <anasilverx@gmail.com>
2023-06-07 22:44:28 -0700

    implemented if else conditionals to change temp display color. registered event listener for changeColorByTemp

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 "use strict";
 
 const state = {
-    temperature: 0
+    temperature: 0 
+    // start temparture with info from API call
 };
 
 const increaseTemp = (event) => {
@@ -17,13 +18,26 @@ const decreaseTemp = (event) => {
 };
 
 const changeColorByTemp = (event) => {
-    // make js to change color using state temperature variable 
+    // make js to change color using state temperature variable
+    const tempCount = document.querySelector('#temp-count');
+    if (state.temperature >= 80) {
+        tempCount.setAttribute("class", "red");
+    } else if (state.temperature >= 70 && state.temperature <= 79) {
+        tempCount.setAttribute("class", "orange");
+    } else if (state.temperature >= 60 && state.temperature <= 69) {
+        tempCount.setAttribute("class", "yellow");
+    } else if (state.temperature >= 50 && state.temperature <= 59) {
+        tempCount.setAttribute("class", "green");
+    } else if (state.temperature <= 49) {
+        tempCount.setAttribute("class", "teal");
+    }
 }
 
 
 const registerEventHandlers = (event) => {
     const tempUpButton = document.querySelector("#up-arrow");
     tempUpButton.addEventListener("click", increaseTemp);
+    tempUpButton.addEventListener("click", changeColorByTemp);
 
     const tempDownButton = document.querySelector("#down-arrow");
     tempDownButton.addEventListener("click", decreaseTemp);
