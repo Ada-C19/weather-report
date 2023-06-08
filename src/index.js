2aad6bf0a4d14947d43db155644470c290d1cb5a
Anh Huynh <avh2@uw.edu>
2023-06-08 16:20:36 -0700

    Modified index.js file and added more functionality and logic to buttons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 // const { default: axios } = require("axios");
 
+// TASK LIST:
+// AH: create helper function to convert into F
+// AH: call helper function and pass its output into state 
+// LP: change logic of both buttons so Reset button has a default city name (e.g., Seattle) (Wave 5)
+// AH + LP: do the sky (Wave 6)
+
 const state = {
   temp: 90,
 };
@@ -51,7 +57,60 @@ function City() {
   const cityName = document.getElementById("cityname").value;
   const cityHeader = document.querySelector("h2");
   cityHeader.textContent = cityName;
+  state.cityName = cityName;
+  findLatitudeAndLongitude();
 }
+
+const findLatitudeAndLongitude = (query) => {
+  console.log("Hi, find latitude and longtitude")
+  let latitude, longitude;
+  axios
+    .get("http://127.0.0.1:5000/location", {
+      params: {
+        q: state.cityName,
+        format: "json",
+      },
+    }).then((response) => {
+      latitude = response.data[0].lat;
+      longitude = response.data[0].lon;
+      state.latitude = latitude;
+      state.longitude = longitude;
+      console.log("success in findLatitudeAndLongitude!", latitude, longitude);
+      findTemperature(latitude, longitude)
+
+    }).catch((error) => {
+        console.log("error in findLatitudeAndLongitude!");
+    }); 
+  };    
+
+      // make the next API call here!
+const findTemperature = (query) => {
+    console.log("find inside temp")
+    console.log(state.latitude, state.longitude)
+    axios
+      .get("http://127.0.0.1:5000/weather", {
+        params: {
+          lat: state.latitude,
+          lon: state.longitude,
+          // units: "imperial",
+          // format: "json",
+      },
+      }).then((response) => {
+        console.log(response)
+        realTimeTemp = response["data"]["main"]["temp"];
+        console.log("success in find realTimeTemp", realTimeTemp);
+        state.temp = realTimeTemp;
+      }).catch((error) => {
+          console.log("error in find realTimeTemp");
+      });     
+    };    
+
+
+
+console.log("Hello Houston")
+
+
+
 
 const registerEventHandlers = () => {
   const buttonDown = document.getElementById("downbtn");
@@ -72,58 +131,3 @@ const registerEventHandlers = () => {
 };
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
-
-
-// const LOCATIONIQ_KEY = "pk.6623ff3b3334090d644e85b10850a67d";
-// const qCity = "Seattle";
-
-const findLatitudeAndLongitude = (query) => {
-  console.log("Hi, find latitude and longtitude")
-  let latitude, longitude;
-  axios
-    .get("http://127.0.0.1:5000/location", {
-      params: {
-        q: "Seattle",
-        format: "json",
-      },
-    }).then((response) => {
-      latitude = response.data[0].lat;
-      longitude = response.data[0].lon;
-      state.latitude = latitude;
-      state.longitude = longitude;
-      console.log("success in findLatitudeAndLongitude!", latitude, longitude);
-      findTemperature(latitude, longitude)
-
-    }).catch((error) => {
-        console.log("error in findLatitudeAndLongitude!");
-    }); 
-
-    
-    
-  };    
-
-      // make the next API call here!
-const findTemperature = (query) => {
-    console.log("find inside temp")
-    console.log(state.latitude, state.longitude)
-    axios
-      .get("http://127.0.0.1:5000/weather", {
-        params: {
-          lat: state.latitude,
-          lon: state.longitude,
-          // units: "imperial",
-          // format: "json",
-      },
-      }).then((response) => {
-        console.log(response)
-        realTimeTemp = response["data"]["main"]["temp"];
-        console.log("success in find realTimeTemp", realTimeTemp);
-      }).catch((error) => {
-          console.log("error in find realTimeTemp");
-      });     
-    };    
-
-
-
-findLatitudeAndLongitude()
-console.log("Hello Houston")
