9a829361a0a7b7907361bd14265ac7677230d323
an2084 <angelanawang47@gmail.com>
2023-06-08 14:19:14 -0400

    Completed Wave 1 - basic wireframe & html built

---
+++ src/index.js
@@ -0,0 +1,5 @@
+//City name search bar updates city name in header
+function myChangeFunction(input) {
+    var output = document.getElementById('search-output');
+    output.value = input.value;
+}
