d260e13e16d9baec951398774c5ef1f37e2a8cbb
Raissa Barbosa <raissaglaucie@icloud.com>
2023-06-07 22:02:34 -0400

    started logic for change letter color

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ let temperature = 60;
 
 // increase and decrease temperature functions
 
+function updateTemperature() {
+	tempValue.textContent = temperature;
+}
+
 increaseTempControl.addEventListener('click', () => {
 	temperature++;
 	updateTemperature();
@@ -19,6 +23,11 @@ decreaseTempControl.addEventListener('click', () => {
 	updateTemperature();
 });
 
-function updateTemperature() {
-	tempValue.textContent = temperature;
-}
+// change letter colors
+
+// if (temperature >= 80 === red')
+// else if (temperature >= 70 === 'orange')
+// else if (temperature >= 60 === 'yellow')
+// else if (temperature >= 50 === 'green') {
+// else
+// ('teal')
