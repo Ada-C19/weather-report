6d72a61912ccee8c5f7aacebfa3c2da1064483bf
Elizabeth Zachary <elizabethzachary96@gmail.com>
2023-06-07 17:12:26 -0700

    implemented functionality for changing header city given city name input

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const state = {
-    city: 'Seattle, WA',
+    city: 'San Francisco, CA',
     temperature: 68,
     latitude: 45.5152,
     longitude: 122.6784,
@@ -17,12 +17,22 @@ const decreaseTemp = () => {
     tempContainer.textContent = state.temperature;
 };
 
+const renameCityHeader = () => {
+    const headerCity = document.querySelector('#headerCityName');
+    const newCity = document.querySelector('#cityNameInput');
+    state.city = newCity.value;
+    headerCity.innerText = state.city;
+};
+
 const registerEventHandlers = () => {
     const increaseTempBtn = document.querySelector('#increaseTempControl');
     increaseTempBtn.addEventListener("click", increaseTemp);
 
     const decreaseTempBtn = document.querySelector('#decreaseTempControl');
     decreaseTempBtn.addEventListener("click", decreaseTemp);
+
+    const cityNameInput = document.querySelector('#cityNameInput');
+    cityNameInput.addEventListener("input", renameCityHeader);
 };
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
