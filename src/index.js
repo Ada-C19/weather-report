c75592b6339edc0c3852de342abf528da1a05e3a
Alejandra <adom2128@gmail.com>
2023-06-08 08:39:58 -0600

    refactors wave 1 and 2 to remove state

--- src/index.js
+++ src/index.js
@@ -1,31 +1,27 @@
-const state = {
-    // HTML elements
-    temperatureLabel: null,
-    temperatureIncreaseButton: null,
-    temperatureDecreaseButton: null,
-    getRealtimeTemperatureButton: null,
-    skyImage: null,
-    landscapeImage: null,
-
-    // data
-    temperatureValue: 72,
-};
+let temperatureLabel = null;
+let temperatureIncreaseButton = null;
+let temperatureDecreaseButton = null;
+let getRealtimeTemperatureButton = null;
+let skyImage = null;
+let landscapeImage = null;
+let cityName = null;
+let temperatureValue = 72;
 
 const showTemp = () => {
-    state.temperatureLabel.textContent = state.temperatureValue;
+    temperatureLabel.textContent = temperatureValue;
 }; 
 
 const setTempLabelColor = (temperature) => {
     if (temperature >= 80) {
-        return state.temperatureLabel.setAttribute("class", "red");
+        return temperatureLabel.setAttribute("class", "red");
     } else if (temperature >= 70) {
-        return state.temperatureLabel.setAttribute("class", "orange");
+        return temperatureLabel.setAttribute("class", "orange");
     } else if (temperature >= 60) {
-        return state.temperatureLabel.setAttribute("class", "yellow");
+        return temperatureLabel.setAttribute("class", "yellow");
     } else if (temperature >= 50) {
-        return state.temperatureLabel.setAttribute("class", "green");
+        return temperatureLabel.setAttribute("class", "green");
     } else {
-        return state.temperatureLabel.setAttribute("class", "teal");
+        return temperatureLabel.setAttribute("class", "teal");
     }
 };
 
@@ -34,7 +30,7 @@ const landscapeImages = {
     70: "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷",
     60: "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃",
     50: "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲",
-}
+};
 
 const setLandscapeImage = temperature => {
     if (temperature >= 80) {
@@ -46,77 +42,41 @@ const setLandscapeImage = temperature => {
     } else {
         return landscapeImages[50];
     }
-}
+};
 
 const updateTemperature = () => {
-    state.temperatureLabel.textContent = state.temperatureValue;
-    state.landscapeImage.textContent = setLandscapeImage(state.temperatureValue);
-    setTempLabelColor(state.temperatureValue);
+    temperatureLabel.textContent = temperatureValue;
+    landscapeImage.textContent = setLandscapeImage(temperatureValue);
+    setTempLabelColor(temperatureValue);
 };
 
 const loadControls = () => {
-    state.temperatureLabel = document.getElementById("tempValue");
-    state.temperatureDecreaseButton = document.getElementById("decreaseTempControl");
-    state.temperatureIncreaseButton = document.getElementById("increaseTempControl");
-    state.getRealtimeTemperatureButton = document.getElementById("currentTempButton");
-    state.skyImage = document.getElementById("sky");
-    state.landscapeImage = document.getElementById("landscape");
+    temperatureLabel = document.getElementById("tempValue");
+    temperatureDecreaseButton = document.getElementById("decreaseTempControl");
+    temperatureIncreaseButton = document.getElementById("increaseTempControl");
+    getRealtimeTemperatureButton = document.getElementById("currentTempButton");
+    skyImage = document.getElementById("sky");
+    landscapeImage = document.getElementById("landscape");
 };
 
 const handleTemperatureDecreaseButtonClick = () => {
-    state.temperatureValue -= 1;
+    temperatureValue -= 1;
     updateTemperature();
-
-
-    // if (state.temperatureValue >= 80) {
-    //     state.temperatureLabel.classList.remove('orange');
-    //     state.temperatureLabel.classList.add('red');
-    // } else if (state.temperatureValue >= 70) {
-    //     state.temperatureLabel.classList.remove('red');
-    //     state.temperatureLabel.classList.add('orange');
-    // } else if (state.temperatureValue >= 60) {
-    //     state.temperatureLabel.classList.remove('orange');
-    //     state.temperatureLabel.classList.add('yellow');
-    // } else if (state.temperatureValue >= 50) {
-    //     state.temperatureLabel.classList.remove('yellow');
-    //     state.temperatureLabel.classList.add('green');
-    // } else {
-    //     state.temperatureLabel.classList.remove('green');
-    //     state.temperatureLabel.classList.add('teal');
-    // }
-}
+};
 
 const handleTemperatureIncreaseButtonClick = () => {
-    state.temperatureValue += 1;
+    temperatureValue += 1;
     updateTemperature();
-    setTemperatureLabel();
-
-    // if (state.temperatureValue >= 80) {
-    //     state.temperatureLabel.classList.remove('orange');
-    //     state.temperatureLabel.classList.add('red');
-    // } else if (state.temperatureValue >= 70) {
-    //     state.temperatureLabel.classList.remove('red');
-    //     state.temperatureLabel.classList.add('orange');
-    // } else if (state.temperatureValue >= 60) {
-    //     state.temperatureLabel.classList.remove('orange');
-    //     state.temperatureLabel.classList.add('yellow');
-    // } else if (state.temperatureValue >= 50) {
-    //     state.temperatureLabel.classList.remove('yellow');
-    //     state.temperatureLabel.classList.add('green');
-    // } else {
-    //     state.temperatureLabel.classList.remove('green');
-    //     state.temperatureLabel.classList.add('teal');
-    // }
-}
+};
 
 const handleGetRealtimeTemperatureButtonClick = () => {
 
-}
+};
 
 const registerEvents = () => {
-    state.temperatureDecreaseButton.addEventListener("click", handleTemperatureDecreaseButtonClick);
-    state.temperatureIncreaseButton.addEventListener("click", handleTemperatureIncreaseButtonClick);
-    state.getRealtimeTemperatureButton.addEventListener("click", handleGetRealtimeTemperatureButtonClick);
+    temperatureDecreaseButton.addEventListener("click", handleTemperatureDecreaseButtonClick);
+    temperatureIncreaseButton.addEventListener("click", handleTemperatureIncreaseButtonClick);
+    getRealtimeTemperatureButton.addEventListener("click", handleGetRealtimeTemperatureButtonClick);
 };
 
 const onLoad = () => {
@@ -126,5 +86,3 @@ const onLoad = () => {
 };
 
 onLoad();
-
-
