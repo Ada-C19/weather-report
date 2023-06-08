e09e053736818bdfb521516a9895ae608ae55868
Raissa Barbosa <raissaglaucie@icloud.com>
2023-06-07 21:02:10 -0400

    Implement the functions to increase and decrease the temperature

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-"use strict";
+'use strict';
 const tempValue = document.getElementById('tempValue');
 const increaseTempControl = document.getElementById('increaseTempControl');
 const decreaseTempControl = document.getElementById('decreaseTempControl');
-const skySelector = document.getElementById('skySelector');
+const skySelect = document.getElementById('skySelect');
 const gardenContent = document.getElementById('gardenContent');
+
 let temperature = 60;
-tempCheck = (temperature) => {
-    if (temperature >= 80) {
-        
-    }
+
+// increase and decrease temperature functions
+
+increaseTempControl.addEventListener('click', () => {
+	temperature++;
+	updateTemperature();
+});
+
+decreaseTempControl.addEventListener('click', () => {
+	temperature--;
+	updateTemperature();
+});
+
+function updateTemperature() {
+	tempValue.textContent = temperature;
+	applyTemperatureColor();
+	updateLandscape();
 }
