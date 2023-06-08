b2e17ea453246db9248f0b0f75159a1d0544df5d
Ariel Williams <arielwilliams121013@gmail.com>
2023-06-08 00:06:15 -0700

    In index.html, added defer attribute for script tags. This solved the issue of the html buttons not existing yet and trying to run an event on non-existing html. In index.js, created tempValue so 72 not hard coded, fixed increaseTemperature and decreaseTemperature functions by transforming str to int. Also unchained the events for upTempArrow and downTempArrow so we can watch for 1 event at a time. Finally, called the registerEventHandlers function.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+// const so 72 is not hard coded, used in increaseTemp & decreaseTemp functions
+const tempValue = document.getElementById('tempValue');
+
+
 // assign a constant state for temp
 const state = {
-    tempCount : 0, 
+    tempCount : tempValue.innerText, 
     city: "Seattle"
 }
 
@@ -21,23 +25,38 @@ const skyEmojis = {
     "3" : "so put it off till it's all working"
 };
 
+
+// DO NOT NEED ANYMORE, BUT LEAVING FOR REVIEW
 // functions to update the number on temp gauge
-const changeTemperature = () => {
-    const tempContainer = document.getElementById("temperature-id");
-    tempContainer.textContent = `${state.tempCount}`;
-    tempRange()
-}
+// const changeTemperature = () => {
+//     const tempContainer = document.getElementById("temperature-id");
+//     tempContainer.textContent = `${state.tempCount}`;
+//     tempRange()
+// }
+
+
 
 // add 1 degree to temp
+// had to transform str to int to +1 properly, otherwise it was 721
+// innerText is a key on the element span with id "tempValue"
+// go to dev tools console and click the up and down arrows, see the console!
 const increaseTemperature = () => {
-    state.tempCount += 1;
-    changeTemperature()
+    state.tempCount = Number(state.tempCount) + 1;
+    tempValue.innerText = state.tempCount;
+    tempRange();
+    console.log(state.tempCount)
+    // changeTemperature()
 };
 
 // subtract 1 degree to temp
+// had to transform str to int to -1 properly
+// innerText is a key on the element span with id "tempValue"
 const decreaseTemperature = () => {
-    state.tempCount -= 1;
-    changeTemperature()
+    state.tempCount = Number(state.tempCount) - 1;
+    tempValue.innerText = state.tempCount;
+    tempRange();
+    console.log(state.tempCount)
+    // changeTemperature()
 };
 
 // function to set ranges for temperature and change colors
@@ -75,32 +94,31 @@ const tempRange = () => {
     }
 }
 
+
 const registerEventHandlers = () => {
 
-// assigns upTempArrow to that specific button
-// document keyword grabs everything in index.html
-// then you run func on document called querySelector
-// then you are grabbing the id of #increaseTempControl
-    const upTempArrow = document.getElementById("#increaseTempControl").addEventListener("click", increaseTemperature);
-    const downTempArrow = document.getElementById("#decreaseTempControl").addEventListener("click", decreaseTemperature);
+// assigns upTempArrow to that specific button. Document keyword grabs everything in index.html
+// then you run func on document called querySelector. Then you are grabbing the id of increaseTempControl
+// Do not need # in front of decreaseTempControl bc .getElementById does it automatically
+const downTempArrow = document.getElementById("decreaseTempControl");
+downTempArrow.addEventListener("click", decreaseTemperature);
 
-// upTempArrow watches for a click
-// set a call back function that handles the click event
-// the event that the event listener is watching for is a click event
-// you can change click to hover, mouse, etc. 
-    // upTempArrow.addEventListener('click', (event) => {
-    //     console.log({ event, upTempArrow })
-    // });
+const upTempArrow = document.getElementById("increaseTempControl");
+upTempArrow.addEventListener("click", increaseTemperature);
 
-    // downTempArrow.addEventListener('click', (event) => {
-    //     console.log({ event, downTempArrow })});
+
+// NOTE FOR CHAINED EVENTS BELOW: YOU CAN'T PULL OUT/LOOK AT 1 SPECIFIC EVENT AT A TIME
+// Because you are storing in memory you can't grab individual events, chaining can be problematic for this reason
+    // const upTempArrow = document.getElementById("#increaseTempControl").addEventListener("click", increaseTemperature);
+    // const downTempArrow = document.getElementById("#decreaseTempControl").addEventListener("click", decreaseTemperature);
+
     
-    updateCityHeader();
-    const updateCity = document.getElementById("cityNameInput");
-    updateCity.addEventListener('input', updateCityHeader)
-    };
+    // updateCityHeader();
+    // const updateCity = document.getElementById("cityNameInput");
+    // updateCity.addEventListener('input', updateCityHeader)
+};
+
+// previously didn't exist, calls function here to register events
+registerEventHandlers();
 
 
-
-// next task is to get the 72 to print out on the console in dev tools
-// then when you have access to it you can update it from there to increase with clicks
