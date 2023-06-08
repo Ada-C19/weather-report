422b72cf54a54fbf62d36adc8ff02b5c3f2e8c63
Rediet Medhane <redietkmedhane@gmail.com>
2023-06-07 17:35:58 -0700

    started color change functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 const tempState = {
     liveTemp : 68
 };
+
+
+let temp = tempState.liveTemp;
+let color = 'yellow';
+if (temp >= 80) {
+    color = 'hot';
+} else if (temp >= 70) {
+    color = 'warm';
+} else if (temp >= 60) {
+    color = 'moderate';
+} else if (temp >= 50) {
+    color = 'cool';
+} else {
+    color = 'cold';
+}
+
+const temperature = document.getElementById('live_temp');
+temperature.className = color;
+temperature.textContent = String(tempState.liveTemp);
+
 const tempIncrease = (event) => {
     
     //temp behavior
@@ -24,5 +44,7 @@ const registerEventHandlers = (event) => {
     const tempDecButton = document.querySelector("#decrease_button");
     tempDecButton.addEventListener("click", tempDecrease);
 }
+
+
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
