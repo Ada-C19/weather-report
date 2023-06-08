9aa2997ff0971e6c9f33ebe1bd468867175fec0f
Ericka <erickamoreno529@gmail.com>
2023-06-08 14:19:55 -0700

    change background color

--- src/index.js
+++ src/index.js
@@ -14,3 +14,29 @@ function decreaseTemperature() {
     const currentTemp = parseInt(tempElement.textContent);
     tempElement.textContent = currentTemp - 1;
 }
+
+// Get the text element
+const tempValueElement = document.getElementById('tempValue');
+
+// Function to change the text color
+const changeTextColor = function(tempValue) {
+    if (tempValue >= 80) {
+        tempValueElement.classList.add("red");
+} else if (69 < newValue < 80 ) {
+    tempValueElement.classList.add("orange");
+} else if (59 < newValue < 70 ) {
+    tempValueElement.classList.add("yellow");
+} else if (49 < newValue < 60) {
+    tempValueElement.classList.add("green");
+} else if (49 <= newValue) {
+    tempValueElement.classList.add("teal");
+}
+}
+
+
+// Temperature (F)	Color
+// 80+	Red
+// 70-79	Orange
+// 60-69	Yellow
+// 50-59	Green
+// 49 or below	Teal
