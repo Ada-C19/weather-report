2019e37472a7390a1d00eada5af95e900825a8b2
Lindsay <lindsaybolzwilson@gmail.com>
2023-06-08 12:12:47 -0400

    bad-commit middle of a debug

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 "use strict";
+const tempVal = parseInt(document.getElementById("temp"));
+const tempContainterVal = document.getElementById("temp-container");
+
+// Wave2
+const changeTempColor = () => {
+
+  if (tempVal > 80) {
+    tempVal.className = 'above80Temp';
+    tempContainterVal.className = 'above80Bk';
+
+  } else if (tempVal < 80 && tempVal > 69) {
+    tempVal.className.add('temp69To80');
+    tempContainterVal.className = 'temp69To80Bk';
+  };
+}
+changeTempColor();
