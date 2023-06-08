3635af0b9867d0893c9a406773889a2127744a38
Carline <carlinesenat@gmail.com>
2023-06-08 00:18:29 -0700

    Wave 2 initialized, working on event handling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
+//Wave 2
+//refactor this
+const increaseTemp = document.getElementById('increaseTempControl');
+const decreaseTemp = document.getElementById('decreaseTempControl');
+const tempValue = document.getElementById('tempValue');
+const skySelect = document.getElementById('skySelect');
+const landscape = document.getElementById('landscape');
+//Event handlers
+let temperature = 70;
+state.increaseTemp.addEventListener('click', changeTemp);
+state.decreaseTemp.addEventListener('click', changeTemp);
+
+
+//changes color + landscape
+const changeTemp = () => {
+    let landscapeEmojis = '';
+    if (tempValue >= 80) {
+        tempValue.style.color = 'red';
+        landscapeEmojis = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂';
+    } else if (tempValue< 80 && tempValue >= 70) {
+        tempValue.style.color = 'orange';
+        landscapeEmojis = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷';
+    } else if (tempValue < 70 && tempValue >= 60) {
+        tempValue.style.color = 'yellow';
+        landscapeEmojis = '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃';
+    } else if (tempValue < 60 && temp >= 50) {
+        tempValue.style.color = 'green';
+        landscapeEmojis = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
+    } else {
+        tempValue.style.color = 'teal';
+        landscapeEmojis = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
+    }
+    landscape.textContent = landscapeEmojis;
+};
 
 // --- Wave 3 --- 
 
