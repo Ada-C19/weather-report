5e2c689b6f81e0498e2981cccf8c01b16e1d2ec6
Sophia Tran <sophiat8832@gmail.com>
2023-06-08 14:56:45 -0700

    implemented increaseTemp and registered event handlers

---
+++ src/index.js
@@ -0,0 +1,52 @@
+const state = {
+    currentTemp: 72
+};
+
+const increaseTemp = () => {
+    state.currentTemp++;
+    const currentTemp = document.querySelector('#display-temperature');
+    console.log(state.currentTemp);
+    currentTemp.textContent = `${state.currentTemp}`;
+    // setTempAndColor();
+};
+
+// const decreaseTemp = () => {
+//     currentTemp.currTemp--;
+//     setTempAndColor();
+// };
+
+// const setTempAndLandscape = () => {
+//     const currentTemp = document.querySelector('#display-temperature');
+//     currentTemp.textContent = `${state.currentTemp}`;
+//     changeTempColor();
+//     landscapeTitle();
+// }
+
+// const changeTempColor = () => {
+//     const tempCurrent = document.getElementById('display-temperature');
+//     let color;
+//     switch (true) {
+//         case currentTemp.currTemp >= 80:
+//             color = 'red';
+//             break;
+//         case currentTemp.currTemp >= 70:
+//             color = 'orange';
+//             break;
+//         case currentTemp.currTemp >= 60:
+//             color = 'yellow';
+//             break; 
+//         case currentTemp.currTemp >= 50:
+//             color = 'green';
+//             break;
+//         default:
+//             color = 'teal';
+//     }
+//     tempCurrent.className = color;
+// };
+
+const registerEventHandlers = () => {
+    const increaseButton = document.getElementById("increase-temp");
+    increaseButton.addEventListener("click", increaseTemp);
+};
+
+document.addEventListener("DOMContentLoaded", registerEventHandlers);
