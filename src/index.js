dc448e8dc06472460e8c325a3d399b8ed2acef99
Kimberly Lantigua <klantigua11@gmail.com>
2023-06-08 13:48:25 -0700

    added a ladnscape object to incorporate in our formatTempAndGarden function. Landscape changes as temp is changed by user.

--- src/index.js
+++ src/index.js
@@ -8,41 +8,64 @@ const state = {
     temp: 70,
 };
 
+const landscapes = { 
+    hot: "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂",  
+    warm: "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷", 
+    cool: "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃", 
+    cold: "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲"
+}
+
 const formatTempAndGarden = () => {
     let temp = state.temp;
     let color = 'green';
+    let landscape = landscapes.warm;
 
     if (temp >= 100) {
         color = 'red';
+        landscape = landscapes.hot;
     } else if (temp >= 95) {
         color = 'orangered';
+        landscape = landscapes.hot;
     } else if (temp >= 86) {
         color = 'orange';
+        landscape = landscapes.hot;
     } else if (temp >= 77) {
         color = 'green';
+        landscape = landscapes.warm;
     } else if (temp >= 68) {
         color = 'darkgreen';
+        landscape = landscapes.warm;
     } else if (temp >= 59) {
         color = 'teal';
+        landscape = landscapes.cool;
     } else if (temp >= 50) {
         color = 'cornflowerblue';
-    } else if (temp >= 41){
+        landscape = landscapes.cool;
+    } else if (temp >= 41) {
         color = 'steelblue';
+        landscape = landscapes.cold;
     } else if (temp >= 32) {
         color = 'royalblue';
+        landscape = landscapes.cold;
     } else if (temp >= 23) {
         color = 'darkblue';
+        landscape = landscapes.cold;
     } else if (temp >= 14) {
         color = 'darkslateblue';
+        landscape = landscapes.cold;
     } else if (temp >= 5) {
         color = '#424040';
+        landscape = landscapes.cold;
     } else {
         color = 'black';
+        landscape = landscapes.cold;
     }
 
     const temperature = document.getElementById('tempValue');
+    const newLandscape = document.getElementById('landscape');
     temperature.style.color = color;
     temperature.textContent = String(state.temp);
+    newLandscape.textContent = landscape;
 };
 
 const increaseTemp = () => {
