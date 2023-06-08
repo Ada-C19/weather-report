93d56be4bdead5d054962e388dffdaa6ae11ae06
Abby Castillo <abby.machines@gmail.com>
2023-06-07 20:03:44 -0700

    refactor comments, change increaseTemperature function to incrementValue()

--- src/index.js
+++ src/index.js
@@ -1,24 +1,18 @@
-// const temperatureContent = document.getElementById("temperature__content");
-
-// let temperature = document.getElementById("tempValue").textContent;
-
-// document.getElementById("tempValue").textContent = 420;
-
 let temperature = document.getElementById("tempValue");
 
 temperature.textContent = 420;
 
-// temperature.textContent += 1;
+const incrementValue = (numberString) => {
+  // cast currentTemp from string to int
+  number = parseInt(numberString);
 
-const increaseTemp = (currentTemp) => {
-  // console.log(temperature.textContent);
-  currentTemp = parseInt(currentTemp);
   console.log(parseInt(temperature.textContent));
   console.log("the function ran");
-  return currentTemp + 1;
+  // return a value
+  return number + 1;
 };
 
-temperature.textContent = increaseTemp(temperature.textContent);
+temperature.textContent = incrementValue(temperature.textContent);
 
 console.log("what's up world");
 const cityNameInput = document.getElementById("cityNameInput");
