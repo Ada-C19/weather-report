4fdd27a54d00db0abae0db43c552176c5225a999
Ericka <erickamoreno529@gmail.com>
2023-06-08 14:43:35 -0700

    change color of tempElement in specified ranges

--- src/index.js
+++ src/index.js
@@ -1,37 +1,45 @@
 "use strict";
 
+const changeTextColor = function(tempValue) {
+    tempValueElement.classList.remove("red", "orange", "green", "teal", "yellow")
+    if (tempValue >= 80) {
+        tempValueElement.classList.add("red");
+    } else if ((69 < tempValue) && (tempValue < 80)) {
+        tempValueElement.classList.add("orange");
+    } else if ((59 < tempValue) && (tempValue < 70)) {
+        tempValueElement.classList.add("yellow");
+    } else if ((49 < tempValue) && (tempValue < 60)) {
+        tempValueElement.classList.add("green");
+    } else {
+        tempValueElement.classList.add("teal");
+    }
+}
+
 document.getElementById('increaseTempButton').addEventListener('click', increaseTemperature);
 document.getElementById('decreaseTempButton').addEventListener('click', decreaseTemperature);
+
 
 function increaseTemperature() {
     const tempElement = document.getElementById('tempValue');
     const currentTemp = parseInt(tempElement.textContent);
     tempElement.textContent = currentTemp + 1;
+    changeTextColor(currentTemp)
 }
 
 function decreaseTemperature() {
     const tempElement = document.getElementById('tempValue');
     const currentTemp = parseInt(tempElement.textContent);
     tempElement.textContent = currentTemp - 1;
+    changeTextColor(currentTemp)
 }
 
-// Get the text element
+
+document.getElementById('tempValue').addEventListener('click', changeTextColor);
+
 const tempValueElement = document.getElementById('tempValue');
 
-// Function to change the text color
-const changeTextColor = function(tempValue) {
-    if (tempValue >= 80) {
-        tempValueElement.classList.add("red");
-} else if (69 < newValue < 80 ) {
-    tempValueElement.classList.add("orange");
-} else if (59 < newValue < 70 ) {
-    tempValueElement.classList.add("yellow");
-} else if (49 < newValue < 60) {
-    tempValueElement.classList.add("green");
-} else if (49 <= newValue) {
-    tempValueElement.classList.add("teal");
-}
-}
+
+
 
 
 // Temperature (F)	Color
