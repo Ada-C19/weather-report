5f8ac6d901b32ed9a3e7d863ceae13538e4d6db7
La Tasha Pollard <latashajpollard@gmail.com>
2023-06-08 12:32:41 -0500

    adds handleTempColors functionality

--- src/index.js
+++ src/index.js
@@ -3,15 +3,33 @@ const state = {
 	tempValue: null,
 	increaseTempControl: null,
 	decreaseTempControl: null,
+	skyContainer: null,
+	landscapeContainer: null,
 };
 
 const increaseTemp = (event) => {
 	++state.tempValueCount;
 	state.tempValue.textContent = state.tempValueCount;
+	handleTempColors();
 };
 const decreaseTemp = (event) => {
 	--state.tempValueCount;
 	state.tempValue.textContent = state.tempValueCount;
+	handleTempColors();
+};
+
+const handleTempColors = () => {
+	if (state.tempValueCount >= 80) {
+		state.tempValue.style.color = "red";
+	} else if (state.tempValueCount >= 70 && state.tempValueCount <= 79) {
+		state.tempValue.style.color = "orange";
+	} else if (state.tempValueCount >= 60 && state.tempValueCount <= 69) {
+		state.tempValue.style.color = "yellow";
+	} else if (state.tempValueCount >= 50 && state.tempValueCount <= 59) {
+		state.tempValue.style.color = "green";
+	} else if (state.tempValueCount <= 49) {
+		state.tempValue.style.color = "teal";
+	}
 };
 
 const registerEvents = () => {
@@ -23,11 +41,14 @@ const loadControls = () => {
 	state.tempValue = document.getElementById("tempValue");
 	state.increaseTempControl = document.getElementById("increaseTempControl");
 	state.decreaseTempControl = document.getElementById("decreaseTempControl");
+	state.skyContainer = document.getElementById("sky");
+	state.landscapeContainer = document.getElementById("landscape");
 };
 
 const onLoaded = () => {
 	loadControls();
 	registerEvents();
+	handleTempColors();
 };
 
 onLoaded();
