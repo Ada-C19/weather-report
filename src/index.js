1f1247da4972ef32b979247f3c528895ec545a4b
Jessie <wajliu3@gmail.com>
2023-06-07 20:49:53 -0700

    update change temp function, add sky feature and city feature, add function to enter and reset city buttons, add some CSS styling

--- src/index.js
+++ src/index.js
@@ -16,3 +16,29 @@ const upTemp = () => {
 }
 
 increaseTemp.addEventListener("click",upTemp)
+
+const decreaseTemp = document.querySelector("#downTempControl")
+
+const downTemp = () => {
+    const changeTemp = document.querySelector("#changeTemp");
+    state.temp -= 1;
+    updateTemp();
+}
+
+decreaseTemp.addEventListener("click",downTemp)
+
+const enterCityName = document.querySelector("#enter")
+const nameInput = document.querySelector("#citynameinput")
+const currentName = document.querySelector("#cityname")
+
+const changeCity = () => {
+    currentName.textContent = nameInput.value;
+}
+enterCityName.addEventListener("click",changeCity)
+
+const resetCityName = document.querySelector("#reset")
+const resetCity = () => {
+    currentName.textContent = "";
+    nameInput.value = "";
+}
+resetCityName.addEventListener("click",resetCity)
