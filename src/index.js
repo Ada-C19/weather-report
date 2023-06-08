0082ed9acbdf53c458c0534562d03e641a3acde6
La Tasha Pollard <latashajpollard@gmail.com>
2023-06-08 11:45:31 -0500

    adds decrease temp functality

--- src/index.js
+++ src/index.js
@@ -1,28 +1,33 @@
 const state = {
-    tempValueCount: 0,
-    tempValue: null,
-    increaseTempControl: null,
-    decreaseTempControl: null,
+	tempValueCount: 0,
+	tempValue: null,
+	increaseTempControl: null,
+	decreaseTempControl: null,
 };
 
 const increaseTemp = (event) => {
-    ++state.tempValueCount;
-    state.tempValue.textContent = state.tempValueCount;
+	++state.tempValueCount;
+	state.tempValue.textContent = state.tempValueCount;
+};
+const decreaseTemp = (event) => {
+	--state.tempValueCount;
+	state.tempValue.textContent = state.tempValueCount;
 };
 
 const registerEvents = () => {
-    state.increaseTempControl.addEventListener("click", increaseTemp);
+	state.increaseTempControl.addEventListener("click", increaseTemp);
+	state.decreaseTempControl.addEventListener("click", decreaseTemp);
 };
 
 const loadControls = () => {
-    state.tempValue = document.getElementById("tempValue");
-    state.increaseTempControl = document.getElementById("increaseTempControl");
-    state.decreaseTempControl = document.getElementById("decreaseTempControl");
+	state.tempValue = document.getElementById("tempValue");
+	state.increaseTempControl = document.getElementById("increaseTempControl");
+	state.decreaseTempControl = document.getElementById("decreaseTempControl");
 };
 
 const onLoaded = () => {
-    loadControls();
-    registerEvents();
+	loadControls();
+	registerEvents();
 };
 
 onLoaded();
