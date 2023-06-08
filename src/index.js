e496407007f4ab51690099027c678856eedb0fb3
Lindsay <lindsaybolzwilson@gmail.com>
2023-06-08 14:09:22 -0400

    Updated a little css

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 "use strict";
-const tempVal = parseInt(document.getElementById("temp"));
-const tempContainterVal = document.getElementById("temp-container");
-
+const tempElement = document.getElementById("temp");
+const tempContainterElement = document.getElementById("temp-container");
+// console.log(typeof tempVal);
 // Wave2
 const changeTempColor = () => {
-
+  const tempVal = parseInt(tempElement.textContent);
   if (tempVal > 80) {
-    tempVal.className = 'above80Temp';
-    tempContainterVal.className = 'above80Bk';
-
+    tempElement.className = 'above80Temp';
+    tempContainterElement.className = 'above80Bk';
   } else if (tempVal < 80 && tempVal > 69) {
-    tempVal.className.add('temp69To80');
-    tempContainterVal.className = 'temp69To80Bk';
+    tempElement.className = 'temp69To80';
+    tempContainterElement.className = 'temp69To80Bk';
+  } else if (tempVal < 70 && tempVal > 59) {
+    tempElement.className = 'temp59To70';
+    tempContainterElement.className = 'temp59To70Bk';
+  } else if (tempVal < 60 && tempVal > 49) {
+    tempElement.className = 'temp49To60';
+    tempContainterElement.className = 'temp49To60Bk';
+  } else if (tempVal < 50) {
+    tempElement.className = 'blow50';
+    tempContainterElement.className = 'blow50Bk';
   };
 }
 changeTempColor();
