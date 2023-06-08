a2ba51c55a5d932c7000bbc2123badfbb7a16617
Ana Villalobos <ana.villalobos309@gmail.com>
2023-06-08 09:44:26 -0700

    Temp ranges changes text color and lanscape

--- src/index.js
+++ src/index.js
@@ -22,25 +22,66 @@ const state = {
 // })
 
 
-
 const increaseTemp = (event) => {
     state.degrees += 1;
-    const degreeCount = document.querySelector('#degrees');
+    // const degreeCount = document.querySelector('#degrees');
+    const degreeCount = document.getElementById('degrees');
     degreeCount.textContent = state.degrees;
+    tempColorLandscape();
 };
 
 const decreaseTemp = (event) => {
     state.degrees -= 1;
-    const degreeCount = document.querySelector('#degrees');
+    // const degreeCount = document.querySelector('#degrees');
+    const degreeCount = document.getElementById('degrees');
     degreeCount.textContent = state.degrees;
+    tempColorLandscape();
+};
+
+
+
+const tempColorLandscape = () => {
+    let temp = state.degrees;
+    let color = 'yellow';
+    let landscape = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
+    if (temp >= 80) {
+        color = 'red';
+        landscape = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂";
+    } else if (temp >= 70) {
+        color = 'orange';
+        landscape = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
+    } else if (temp >= 60) {
+        color = 'yellow';
+        landscape = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
+    } else if (temp >= 50) {
+        color = 'green';
+        landscape = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+    } else {
+        color = 'teal';
+        landscape = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+    }
+
+    const tempNum = document.getElementById('degrees');
+    tempNum.style.color = color;
+
+    const newLandscape = document.getElementById('landscape-emojis');
+    newLandscape.textContent = landscape;
+
 };
 
 const registerEventHandlers = (event) => {
-    const upButton = document.querySelector('#up');
+    tempColorLandscape();
+
+    // const upButton = document.querySelector('#up');
+    const upButton = document.getElementById('up');
     upButton.addEventListener("click", increaseTemp);
 
-    const downButton = document.querySelector('#down');
+
+    // const downButton = document.querySelector('#down');
+    const downButton = document.getElementById('down');
     downButton.addEventListener("click", decreaseTemp);
+
+
 };
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
