e4f67696ae99ddd4194c11b2e9d9d7b16d68232f
Ericka <erickamoreno529@gmail.com>
2023-06-08 13:37:00 -0700

    enable temp increase and decrease

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 "use strict";
+
+document.getElementById('increaseTempButton').addEventListener('click', increaseTemperature);
+document.getElementById('decreaseTempButton').addEventListener('click', decreaseTemperature);
+
+function increaseTemperature() {
+    const tempElement = document.getElementById('tempValue');
+    const currentTemp = parseInt(tempElement.textContent);
+    tempElement.textContent = currentTemp + 1;
+}
+
+function decreaseTemperature() {
+    const tempElement = document.getElementById('tempValue');
+    const currentTemp = parseInt(tempElement.textContent);
+    tempElement.textContent = currentTemp - 1;
+}
