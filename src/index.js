9991e08b410a635c109711f38ace3f0588cff68b
Ericka <erickamoreno529@gmail.com>
2023-06-09 11:55:47 -0700

    Header city name updates with input bar event
    Co-authored-by: Lemmi <Lemmi-C@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -71,18 +71,45 @@ const changeSky = function(event) {
     } 
 }
 document.getElementById('skySelect').addEventListener('change', changeSky);
-
-
-
-
-// function changeSky() {
-//     var x = document.getElementById("skySelect").value;
-//     document.getElementById("skySelect").innerHTML = "You selected: " + x;
-// }
-
-
-
 // END OF SKY CHANGE FUNCTION //
+
+
+// Changing the city name in the header to input city name
+
+const inputName = document.getElementById('cityNameInput');
+const headerCityName = document.getElementById('headerCityName');
+    
+
+const updateCityName = function(event) {
+    headerCityName.textContent = event.target.value 
+    
+    // state.city = inputName;
+    // headerCityName.textContent = state.city;
+};
+document.getElementById('cityNameInput').addEventListener('input', updateCityName)
+// END OF CHANGING CITY IN HEADER //
+
+
+// API CALLS :) ////
+// id="cityNameInput" --> 
+// id="cityNameReset" >
+document.getElementById('cityNameInput').addEventListener('change', changeSky);
+document.getElementById('cityNameReset').addEventListener('click', changeSky);
+const ...
+const ...
+
+axios
+.get('127.0.0.1:5000')
+// add parameters for city info
+.then((response) => {
+    console.log('Success!', response.data);
+    return response.data
+    // Code that executes with a successful response goes here
+})
+.catch((error) => {
+    // Code that executes with an unsuccessful response goes here
+});
+// END OF API CALL //
 
 
 // making an api call
