3a20b43f7ecf7ce6955a447a720d4bc03dd292f9
Melinda Mott <melinda.mott@gmail.com>
2023-06-09 12:13:42 -0700

    Made changes to api call and get weather button, city button done

--- src/index.js
+++ src/index.js
@@ -4,18 +4,23 @@ const state = {
 city: 'Seattle',
 lat: 47.6038321,
 long: -122.3300624,
-temp: 72,
+temp: 65,
 };
 
 const convertKtoF = (temp) => {
   return (temp - 273.15) * (9 / 5) + 32;
 };
 
+const LOCATION_KEY = process.env.LOCATION_KEY;
+const WEATHER_KEY = process.env.WEATHER_KEY;
+
+
 const findLatAndLong = () => {
   //let lat, long;
 axios
-.get('https://api.openweathermap.org/data/2.5/weather', {
+.get('https://us1.locationiq.com/v1/search.php', {
     params: {
+    key: LOCATION_KEY,
     q: state.city,
     },
     })
@@ -31,9 +36,11 @@ axios
 };
 
 const getWeather = () => {
+    const WEATHER_KEY = process.env.WEATHER_KEY;
 axios
-    .get('https://us1.locationiq.com/v1/search.php', {
+    .get('https://api.openweathermap.org/data/2.5/weather', {
     params: {
+        key: WEATHER_KEY,
         lat: state.lat,
         lon: state.long,
 },
@@ -48,17 +55,30 @@ axios
     });
 };
 
+// Does the same as below but wasn't working, so I tried this. Keep this till later in case we need it. 
+// document.addEventListener('DOMContentLoaded', function () {
+//     const button = document.getElementById('getWeatherButton');
+//     button.addEventListener('click', function () {
+//         findLatAndLong();
+//     });
+// });
+
 const updateCityName = () => {
     const inputName = document.getElementById('cityNameInput').value;
-    const headerCityName = document.getElementById('headerCityName');
+    const cityName = document.getElementById('cityName');
     state.city = inputName;
-    headerCityName.textContent = state.city;
+    cityName.textContent = state.city;
 };
     
 const resetCityName = () => {
     const cityNameInput = document.getElementById('cityNameInput');
     cityNameInput.value = 'Seattle'; 
     updateCityName();
+};
+
+function formatTempAndLandscape() {
+    const temperatureElement = document.getElementById('tempValue');
+    temperatureElement.textContent = state.temp + '°F';
 };
 
 const increaseTemp = () => {
@@ -76,7 +96,13 @@ const registerEventHandlers = () => {
 
     const cityNameResetBtn = document.getElementById('cityNameReset');
     cityNameResetBtn.addEventListener('click', resetCityName);
-
 };
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
+
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.getElementById('currentTempButton');
+    button.addEventListener('click', function () {
+        findLatAndLong();
+    });
+});
