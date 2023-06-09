ab7921ae03609c6643294902070b472509cb611f
Elaine Watkins <ewatki.ga@gmail.com>
2023-06-09 12:17:15 -0400

    added change_color function to change color of temperature number if in a range

--- src/index.js
+++ src/index.js
@@ -3,43 +3,69 @@ const state = {
     right_arrow: null,
     degreeCountLabel: null,
     degreeCount: 70
-  }
-  
-  const refreshUI = () => {
+}
+
+const refreshUI = () => {
     state.degreeCountLabel.textContent = state.degreeCount;
-  }
-  
-  let decreaseDegree = (event) => {
+}
+
+const change_color = () => {
+    if (state.degreeCountLabel.textContent >= 80) 
+    {
+        state.degreeCountLabel.style.color = 'red';
+    } else if (state.degreeCountLabel.textContent >= 70) {
+        state.degreeCountLabel.style.color = 'orange';
+    } else if (state.degreeCountLabel.textContent >= 60) 
+    {
+        state.degreeCountLabel.style.color = 'yellow';
+    } else if (state.degreeCountLabel.textContent >= 50) {
+        state.degreeCountLabel.style.color = 'green';
+        console.log('green')
+    } else if (state.degreeCountLabel.textContent <= 49) {
+        state.degreeCountLabel.style.color = 'teal';
+        console.log('teal')
+    }
+}
+
+let decreaseDegree = (event) => {
     --state.degreeCount;
+    change_color();
     refreshUI();
-  }
-  
-  let increaseDegree = (event) => {
+}
+
+let increaseDegree = (event) => {
     ++state.degreeCount;
+    change_color();
     refreshUI();
-  }
-  
-  const loadControls = () => {
+}
+
+const temperature_color = (event) => {
+    if (state.degreeCount <= 49) {
+        state.degreeCount.style.color = teal;
+    }
+}
+
+const loadControls = () => {
     state.degreeCountLabel = document.getElementById('degrees')
     state.left_arrow = document.querySelector('.fa-chevron-left');
     state.right_arrow = document.querySelector('.fa-chevron-right');
-  }
-  
-  const registerEvents = () => {
+}
+
+const registerEvents = () => {
     state.left_arrow.addEventListener('click', decreaseDegree);
     state.right_arrow.addEventListener('click', increaseDegree);
-  }
-  
-  const onLoaded = () => {
+}
+
+const onLoaded = () => {
     // steps to carry out when the page has loaded
     loadControls();
     registerEvents();
     refreshUI();
-  };
-  
+};
+
   // we would usually run the onLoaded function in
   // response to the document finishing loading, but in
   // code sandbox, the page has already loaded by the
   // time our script is added to the page.
   // document.addEventListener("DOMContentLoaded", onLoaded);
-  onLoaded();
+onLoaded();
