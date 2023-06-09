7af4365d3c52ca356547a96ed95db71bff8497f7
Lindsay <lindsaybolzwilson@gmail.com>
2023-06-08 21:01:14 -0400

    Renaming and factoring images for wave 2

--- src/index.js
+++ src/index.js
@@ -1,25 +1,63 @@
 "use strict";
-const tempElement = document.getElementById("temp");
-const tempContainterElement = document.getElementById("temp-container");
-// console.log(typeof tempVal);
+
+const state = {
+  tempEl: document.getElementById("temp"),
+  tempConEl: document.getElementById("temp-container"),
+  tempIncrease: document.getElementById("temp-increase"),
+  tempDecrease: document.getElementById("temp-decrease"),
+  tempVal: parseInt(document.getElementById("temp").textContent),
+  landscapeEl: document.getElementById("landscape"),
+}
+
 // Wave2
 const changeTempColor = () => {
-  const tempVal = parseInt(tempElement.textContent);
-  if (tempVal > 80) {
-    tempElement.className = 'above80Temp';
-    tempContainterElement.className = 'above80Bk';
-  } else if (tempVal < 80 && tempVal > 69) {
-    tempElement.className = 'temp69To80';
-    tempContainterElement.className = 'temp69To80Bk';
-  } else if (tempVal < 70 && tempVal > 59) {
-    tempElement.className = 'temp59To70';
-    tempContainterElement.className = 'temp59To70Bk';
-  } else if (tempVal < 60 && tempVal > 49) {
-    tempElement.className = 'temp49To60';
-    tempContainterElement.className = 'temp49To60Bk';
-  } else if (tempVal < 50) {
-    tempElement.className = 'blow50';
-    tempContainterElement.className = 'blow50Bk';
+  if (state.tempVal > 80) {
+    state.tempEl.className = 'above80Temp';
+    state.tempConEl.className = 'above80Bk';
+  } else if (state.tempVal < 80 && state.tempVal > 69) {
+    state.tempEl.className = 'temp69To80';
+    state.tempConEl.className = 'temp69To80Bk';
+  } else if (state.tempVal < 70 && state.tempVal > 59) {
+    state.tempEl.className = 'temp59To70';
+    state.tempConEl.className = 'temp59To70Bk';
+  } else if (state.tempVal < 60 && state.tempVal > 49) {
+    state.tempEl.className = 'temp49To60';
+    state.tempConEl.className = 'temp49To60Bk';
+  } else if (state.tempVal < 50) {
+    state.tempEl.className = 'blow50';
+    state.tempConEl.className = 'blow50Bk';
   };
 }
-changeTempColor();
+
+const changeLandscape = () => {
+  const landscapeImage = document.getElementById("landscape-img");
+  if (state.tempVal >= 80) {
+    landscapeImage.src = 'assets/landscapes/dunes.jpg';
+    landscapeImage.alt = 'Desert dunes landscape';
+  } else if (state.tempVal < 80 && state.tempVal >= 70) {
+    landscapeImage.src = 'assets/landscapes/grass.jpg';
+    landscapeImage.alt = 'Happy cow in lush grassy green hills landscape';
+  } else if (state.tempVal < 70 && state.tempVal >= 33) {
+    landscapeImage.src = 'assets/landscapes/forest.jpg';
+    landscapeImage.alt = 'Foggy dense forest landscape';
+  } else {
+    landscapeImage.src = 'assets/landscapes/tundra.jpg';
+    landscapeImage.alt = 'Icy expansive landscape';
+  }
+}
+
+state.tempIncrease.addEventListener('click', (event) => {
+  event.preventDefault()
+  state.tempVal += 1;
+  state.tempEl.textContent = state.tempVal;
+  changeTempColor();
+  changeLandscape();
+})
+
+state.tempDecrease.addEventListener('click', (event) => {
+  event.preventDefault()
+  state.tempVal -= 1;
+  state.tempEl.textContent = state.tempVal;
+  changeTempColor();
+  changeLandscape();
+})
