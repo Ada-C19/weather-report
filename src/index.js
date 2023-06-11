8b04d22775aa18437df6268ee70c0c899a4e73f6
Gloria <gloria.wan.glory@gmail.com>
2023-06-10 21:13:50 -0400

    styling dropdown button. Still need to work on dropdown event

--- src/index.js
+++ src/index.js
@@ -7,7 +7,32 @@ searchButton.addEventListener("click", () => {
 });
 
 
-const dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'));
-const dropdownList = dropdownElementList.map((dropdownToggleEl) => {
-  return new mdb.Dropdown(dropdownToggleEl);
+// const dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'));
+// const dropdownList = dropdownElementList.map((dropdownToggleEl) => {
+//   return new mdb.Dropdown(dropdownToggleEl);
+// });
+
+// 
+// Sky DROPDOWN BUTTON
+const dropdownBtn = document.getElementsByClassName("dropdown-button");
+const dropdownMenu = document.getElementsByClassName("dropdown-menu");
+const toggleArrow = document.getElementById("arrow");
+
+// Toggle dropdown function
+const toggleDropdown = function () {
+  dropdownMenu.classList.toggle("show");
+  toggleArrow.classList.toggle("arrow");
+};
+
+// Toggle dropdown open/close when dropdown button is clicked
+dropdownBtn.addEventListener("click", function (e) {
+  e.stopPropagation();
+  toggleDropdown();
+});
+
+// Close dropdown when dom element is clicked
+document.documentElement.addEventListener("click", function () {
+  if (dropdownMenu.classList.contains("show")) {
+    toggleDropdown();
+  }
 });
