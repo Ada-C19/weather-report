7814760fcf7b7c1bdf088b40d60004d99629167d
Rainacam <rainacam12@gmail.com>
2023-06-11 17:52:25 -0400

    increase temp button works as well as color-changing feature

--- src/index.js
+++ src/index.js
@@ -1,90 +1,133 @@
 // actions for search/reset button for city input
-const searchButton = document.getElementById("search-button");
-const searchInput = document.getElementById("search-input");
-searchButton.addEventListener("click", () => {
-  const inputValue = searchInput.value;
-  alert(inputValue);
-});
+document.addEventListener("DOMContentLoaded", function () {
+    const searchButton = document.getElementById("search-button");
+    const searchInput = document.getElementById("search-input");
+    searchButton.addEventListener("click", () => {
+    const inputValue = searchInput.value;
+    alert(inputValue);
+    });
 
-// TEMPERATURE - Change Temperature
-// increase temperature 
-const increaseTemp = document.querySelector("#increase-temp");
+    // TEMPERATURE - Change Temperature
+    // increase temperature 
 
-increaseTemp.addEventListener("click", function() {
-  increaseTemp.textContent += 1;
-});
+        
+    const increaseTemp = document.querySelector("#increase-temp");
+    const decreaseTemp = document.querySelector("#decrease-temp")
+    const displayTemp = document.getElementById("display-temp");
+    let temperature = 65;
 
-// SKY DROPDOWN ATTEMPT 2
-//https://alvarotrigo.com/blog/drop-down-menu-javascript/
-// get all dropdown from document
-document.querySelectorAll('.dropdown-button').forEach(dropDownFunc);
+    const updateTemp = () =>  {
+        displayTemp.textContent = temperature;
+        updateTempColor();
+    }
+
+    function updateTempColor() {
+        if (temperature >= 80) {
+        displayTemp.style.color = "red";
+        } else if (temperature >= 70 && temperature <= 79) {
+        displayTemp.style.color = "orange";
+        } else if (temperature >= 60 && temperature <= 69) {
+        displayTemp.style.color = "yellow";
+        } else if (temperature >= 50 && temperature <= 59) {
+        displayTemp.style.color = "green";
+        } else {
+        displayTemp.style.color = "teal";
+        }
+    }
+
+    // increaseTemp.addEventListener("click", function() {
+    //     increaseTemp.textContent += 1;
+    // });
+
+
+    function increaseTemperature() {
+        temperature++;
+        updateTemp();
+    }
+
+    function decreaseTemperature() {
+        decreaseTemp--;
+        updateTemp();
+    }  
+    increaseTemp.addEventListener("click", increaseTemperature);
+    decreaseTemp.addEventListener("click", decreaseTemperature);
+
+    updateTemp();
+
+
+    // SKY DROPDOWN ATTEMPT 2
+    //https://alvarotrigo.com/blog/drop-down-menu-javascript/
+    // get all dropdown from document
+    document.querySelectorAll('.dropdown-button').forEach(dropDownFunc);
 
 // drop down open and close
-function dropDownFunc(dropDown) {
+    function dropDownFunc(dropDown) {
     console.log(dropDown.classList.contains('click-dropdown'));
 
     if(dropDown.classList.contains('click-dropdown')=== true){
-      dropDown.addEventListener('click', function (e) {
+        dropDown.addEventListener('click', function (e) {
         e.preventDefault();      
 
-        if (this.nextElementSibling.classList.contains('dropdown-active') === true) {
-          // Close the clicked dropdown
-          this.parentElement.classList.remove('dropdown-open');
-          this.nextElementSibling.classList.remove('dropdown-active');
-        } else {
-          // Close the opened dropdown
-          closeDropdown();
-            
-          // add the open and active class(Opening the DropDown)
-          this.parentElement.classList.add('dropdown-open');
-          this.nextElementSibling.classList.add('dropdown-active');
-        }
-      });
+    if (this.nextElementSibling.classList.contains('dropdown-active') === true) {
+        // Close the clicked dropdown
+        this.parentElement.classList.remove('dropdown-open');
+        this.nextElementSibling.classList.remove('dropdown-active');
+    } else {
+        // Close the opened dropdown
+        closeDropdown();
+        
+        // add the open and active class(Opening the DropDown)
+        this.parentElement.classList.add('dropdown-open');
+        this.nextElementSibling.classList.add('dropdown-active');
+    }
+    });
     }
 
     if(dropDown.classList.contains('hover-dropdown') === true){
-    
+
     dropDown.onmouseover  =  dropDown.onmouseout = dropdownHover;
 
     function dropdownHover(e){
-      if(e.type == 'mouseover'){
+        if(e.type == 'mouseover'){
         // Close the opened dropdown
         closeDropdown();
 
         // add the open and active class(Opening the DropDown)
         this.parentElement.classList.add('dropdown-open');
         this.nextElementSibling.classList.add('dropdown-active');
-      }
+            }
+        }
     }
-  }
-};
+    };
 
-//listen to the doc click
-window.addEventListener('click', function(e) {
+    //listen to the doc click
+    window.addEventListener('click', function(e) {
 
-  //close the menu if click happen outside of menu
-  if (e.target.closest('.dropdown') === null) {
+    //close the menu if click happen outside of menu
+    if (e.target.closest('.dropdown') === null) {
     //close the opend dropdown
     closeDropdown();
-  }
-});
+    }
+    });
 
-// Close the opened Dropdowns
-function closeDropdown() { 
-  console.log('closed');
-  
-  // remove the open and active class from other opened Dropdown (Closing the opend DropDown)
-  document.querySelectorAll('.dropdown').forEach(function (container) { 
-      container.classList.remove('dropdown-open')
-  });
+    // Close the opened Dropdowns
+    function closeDropdown() { 
+    console.log('closed');
 
-  document.querySelectorAll('.dropdown-menu').forEach(function (menu) { 
-      menu.classList.remove('dropdown-active');
-  });
-}
+    // remove the open and active class from other opened Dropdown (Closing the opend DropDown)
+    document.querySelectorAll('.dropdown').forEach(function (container) { 
+        container.classList.remove('dropdown-open')
+    });
 
-// close the dropdown on mouse out from the dropdown list
-document.querySelectorAll('.dropdown-menu').forEach(function (dropDownList) { 
-  // close the dropdown after user leave the list
-  dropDownList.onmouseleave = closeDropdown;
-});
+    document.querySelectorAll('.dropdown-menu').forEach(function (menu) { 
+        menu.classList.remove('dropdown-active');
+    });
+    }
+
+    // close the dropdown on mouse out from the dropdown list
+    document.querySelectorAll('.dropdown-menu').forEach(function (dropDownList) { 
+    // close the dropdown after user leave the list
+    dropDownList.onmouseleave = closeDropdown;
+    })
+
+    });
