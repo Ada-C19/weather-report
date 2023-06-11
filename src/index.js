853b5505bef853bbc8a291d544244b947cfb4b3b
Abby <argoodm@gmail.com>
2023-06-11 19:31:26 -0400

    corrected button styling for up and down arrows

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+// select the hml element the event has to occur on 
+// make function to run when it occurs
+//register function in event list
+
 let temp = 50
 
 const incrementTemp = function() {
