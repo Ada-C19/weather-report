f4926471f5933a48df2eba0c0baffd61de0d1249
Abby <argoodm@gmail.com>
2023-06-11 19:57:18 -0400

    wave 1 and 2 completed, arrow funcitonality

--- src/index.js
+++ src/index.js
@@ -2,13 +2,28 @@
 // make function to run when it occurs
 //register function in event list
 
-let temp = 50
+ state = {
+    temp: 50,
+};
 
 const incrementTemp = function() {
-    let currentTemp = temp += 1;
-    return currentTemp
-} 
+    state.temp +=1;
+    const tempNumContainer = document.querySelector("#currentTemp")
+    tempNumContainer.textContent = `${state.temp}°`;
+};
 const decrementTemp = function() {
-    let currentTemp = temp -= 1;
-    return currentTemp
-} 
+    state.temp -=1
+    const tempNumContainer = document.querySelector("#currentTemp")
+    tempNumContainer.textContent = `${state.temp}°`
+    
+};
+
+const registerEventHandlers = () => {
+    const upArrow = document.querySelector("#up_arrow");
+    upArrow.addEventListener('click', incrementTemp);
+
+    const DownArrow = document.querySelector("#down_arrow");
+    DownArrow.addEventListener('click', decrementTemp);
+};
+
+document.addEventListener("DOMContentLoaded", registerEventHandlers);
