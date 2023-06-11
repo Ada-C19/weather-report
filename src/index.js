74b58ae40adf0108c1baaecaf5198b228ac7ece0
Setharika Sok <soksetharika7@gmail.com>
2023-06-11 00:31:09 -0400

    Wave 2 created increment and decrement button functions and color change on counter number.

--- src/index.js
+++ src/index.js
@@ -3,3 +3,51 @@ function myChangeFunction(input) {
     var output = document.getElementById('search-output');
     output.value = input.value;
 }
+// count up 
+const state = {
+    tempCount: 0
+};
+
+function updateColor() {
+    if (state.tempCount >= 80) {
+        state.color = "red";
+    }
+    else if (state.tempCount >= 70) {
+        state.color = "orange";
+    }
+    else if (state.tempCount >= 60) {
+        state.color = "yellow";
+    }
+    else if (state.tempCount >= 50) {
+        state.color = "green";
+    }
+    else {
+        state.color= "black";
+    }
+    document.querySelector("#count").style.color = state.color
+    console.log("Updated color " + state.color)
+}
+
+
+const upButton = document.querySelector("#button-up");
+upButton.addEventListener("click", upFunction)
+
+function upFunction() {
+    
+    state.tempCount++;
+    // alert("Up" + state.tempCount)
+    
+    document.querySelector("#count").innerHTML = state.tempCount;
+    updateColor()
+}
+
+const downButton = document.querySelector("#button-down");
+downButton.addEventListener("click", downFunction)
+
+function downFunction() {
+    state.tempCount--;
+    // alert("Down" + state.tempCount)
+    document.querySelector("#count").innerHTML = state.tempCount;
+    updateColor()
+}
+
