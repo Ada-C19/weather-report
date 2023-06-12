9a3757880bf05824247b179db7c2e2dcae4821df
An Ba <ngwin.ab@gmail.com>
2023-06-12 15:46:03 -0400

    displayed city name

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 "use strict";
+import axios, { isCancel, AxiosError } from 'axios';
+
+const addCity = () => {
+    document.getElementById('city-name').innerText = "Real-time weather in lovely " + document.getElementById('search-text-box').value
+}
+
+const registerCitySearchEvent = () => {
+    const citySearchButton = document.querySelector("#city-search");
+    citySearchButton.addEventListener("click", addCity);
+};
+
+document.addEventListener("DOMContentLoaded", registerCitySearchEvent);
+
+axios
+    .get('https://us1.locationiq.com/v1/search?key=YOUR_ACCESS_TOKEN&q=SEARCH_STRING&format=json')
+    .then((response) => {
+        
+    })
+    .catch((error) => {
+        
+    });
+
+
