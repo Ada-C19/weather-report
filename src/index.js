98ed9d4de07219ebf3302772794e6008eda18e84
An Ba <ngwin.ab@gmail.com>
2023-06-12 22:17:23 -0400

    modified functions for better readability

--- src/index.js
+++ src/index.js
@@ -1,24 +1,38 @@
 "use strict";
-import axios, { isCancel, AxiosError } from 'axios';
+// import axios, { isCancel, AxiosError } from 'axios';
 
-const addCity = () => {
-    document.getElementById('city-name').innerText = "Real-time weather in lovely " + document.getElementById('search-text-box').value
-}
-
-const registerCitySearchEvent = () => {
-    const citySearchButton = document.querySelector("#city-search");
-    citySearchButton.addEventListener("click", addCity);
+const state = {
+    citySearchButton: null,
 };
 
-document.addEventListener("DOMContentLoaded", registerCitySearchEvent);
+const handleSearchButtonClicked = () => {
+    const cityHeader = document.getElementById('city-header');
+    const searchInput = document.getElementById('search-input').value;
+    cityHeader.textContent = `Real-time weather in lovely ${searchInput}`;
+};
 
-axios
-    .get('https://us1.locationiq.com/v1/search?key=YOUR_ACCESS_TOKEN&q=SEARCH_STRING&format=json')
-    .then((response) => {
-        
-    })
-    .catch((error) => {
-        
-    });
+const registerEvents = () => {
+    state.citySearchButton.addEventListener("click", handleSearchButtonClicked);
+};
+
+const loadControls = () => {
+    state.citySearchButton = document.getElementById("city-search-btn");
+};
+
+const onLoad = () => {
+    loadControls();
+    registerEvents();
+};
+
+onLoad();
+
+// axios
+//     .get('https://us1.locationiq.com/v1/search?key=YOUR_ACCESS_TOKEN&q=SEARCH_STRING&format=json')
+//     .then((response) => {
+
+//     })
+//     .catch((error) => {
+
+//     });
 
 
