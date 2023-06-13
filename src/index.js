e12920f32a8bbce66f29ef38771c39df6ee772f7
An Ba <ngwin.ab@gmail.com>
2023-06-13 08:29:46 -0400

    added functions and eventListener to display location and realtime weather, incorporated codes for changing temperature

--- src/index.js
+++ src/index.js
@@ -4,72 +4,100 @@
 
 const state = {
     citySearchButton: null,
+    tempValue: 72,
+    lat: null,
+    lon: null,
+    upbutton: null,
+    downbutton: null
+};
+
+const changeCityHeader = () => {
+    const cityName = document.getElementById("city-spotlight");
+    const searchInput = document.getElementById("search-input").value;
+    cityName.innerHTML = `${searchInput}`;
+};
+
+const getLocation = () => {
+    let lat;
+    let lon;
+    return axios.get("http://127.0.0.1:5000/location", {
+        params: {
+            q: document.getElementById("search-input").value
+        }
+    })
+        .then((response) => {
+            state.lat = response.data[0].lat;
+            state.lon = response.data[0].lon;
+            getRtWeather();
+        })
+        .catch((error) => {
+            console.log("There's an error " + error);
+        });
+};
+
+const getRtWeather = () => {
+    axios.get("http://127.0.0.1:5000/weather",
+    {
+        params: {
+            lat: state.lat,
+            lon: state.lon,
+            units: "imperial"
+        }
+    })
+    .then((response) => {
+        state.tempValue = response.data.main.temp;
+        document.getElementById("temp-value").innerHTML = state.tempValue;
+    })
+
+    .catch((error) => {
+        console.log("There's an error " + error);
+    })
 };
 
 const handleSearchButtonClicked = () => {
-    const cityHeader = document.getElementById('city-spotlight');
-    const searchInput = document.getElementById('search-input').value;
-    cityHeader.textContent = `Real-time weather in lovely ${searchInput}`;
+    changeCityHeader();
+    getLocation();
 };
+
+const handleUpBtnClicked = () => {
+    state.tempValue += 1;
+    console.log(state.tempValue);
+
+    document.getElementById("temp-value").innerHTML = state.tempValue;
+}
+
+const handleDownBtnClicked = () => {
+    state.tempValue -= 1;
+    console.log(state.tempValue);
+
+    document.getElementById("temp-value").innerHTML = state.tempValue;
+}
 
 const registerEvents = () => {
     state.citySearchButton.addEventListener("click", handleSearchButtonClicked);
+    state.downbutton.addEventListener("click", handleDownBtnClicked);
+    state.upbutton.addEventListener("click", handleUpBtnClicked);
+};
+
+const refreshUI = () => {
+    document.getElementById("temp-value").innerHTML = 72;
 };
 
 const loadControls = () => {
-    state.citySearchButton = document.getElementById("city-search-btn");
+    state.citySearchButton = document.getElementById("search-button");
+    // state.tempValue = document.getElementById("temp-value"); 
+    state.upbutton = document.getElementById("up-button");
+    state.downbutton = document.getElementById("down-button");
 };
 
 const onLoad = () => {
     loadControls();
     registerEvents();
+    refreshUI();
+    console.log(state.tempValue);
 };
 
-// onLoad();
-
-// axios
-//     .get('https://us1.locationiq.com/v1/search?key=YOUR_ACCESS_TOKEN&q=SEARCH_STRING&format=json')
-//     .then((response) => {
-
-//     })
-//     .catch((error) => {
-
-//     });
-
-
-
-
-let citytemp = 72
-
-// Increase button
-
-const upbutton = document.getElementById("up-button");
-
-upbutton.addEventListener(
-    "click", (event) => {
-        // console.log("Yay it works!")
-        citytemp += 1
-        console.log(citytemp)
-
-        document.getElementById("temp-number").innerHTML = citytemp;
-    }
-);
-
-// Decrease button
-const downbutton = document.getElementById("down-button");
-
-downbutton.addEventListener(
-    "click", (event) => {
-        // console.log("Yay this also works!")
-        citytemp -= 1
-        console.log(citytemp)
-        document.getElementById("temp-number").innerHTML = citytemp;
-
-    }
-);
-
-
-
+onLoad();
 
 // Sky Condition Weather Icon Garden
 const skyicon = document.getElementById("weather-conditions");
@@ -120,3 +148,33 @@ skyicon.addEventListener(
 
     }
 )
+
+
+// let citytemp = 72
+
+// Increase button
+
+// const upbutton = document.getElementById("up-button");
+
+// upbutton.addEventListener(
+//     "click", (event) => {
+//         // console.log("Yay it works!")
+//         citytemp += 1
+//         console.log(citytemp)
+
+//         document.getElementById("temp-number").innerHTML = citytemp;
+//     }
+// );
+
+// // Decrease button
+// const downbutton = document.getElementById("down-button");
+
+// downbutton.addEventListener(
+//     "click", (event) => {
+//         // console.log("Yay this also works!")
+//         citytemp -= 1
+//         console.log(citytemp)
+//         document.getElementById("temp-number").innerHTML = citytemp;
+
+//     }
+// );
