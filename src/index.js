530d5ac163fd26d55fb0cb3575bbad9db2ac18b1
Gloria <gloria.wan.glory@gmail.com>
2023-06-13 14:33:08 -0400

    draft of calling api long/lad to location -> weather

--- src/index.js
+++ src/index.js
@@ -107,7 +107,6 @@ document.addEventListener("DOMContentLoaded", function () {
         cityName.textContent = defaultCity;
     } )
     
-
     // search button 
     const searchButton = document.querySelector("#search-button");
 
@@ -122,9 +121,58 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
+// -------- wave 4: calling api  ---------
+const LOCATIONIQ_KEY = process.env['api_key'];
 
+const findLatitudeAndLongitude = (query) => {
+let latitude, longitude;
+axios.get('https://us1.locationiq.com/v1/search.php',
+{
+    params: {
+    key: LOCATIONIQ_KEY,
+    q: 'Seattle, Washington, USA',
+    format: 'json'
+    }
+})
+.then( (response) => {
+    latitude = response.data[0].lat;
+    longitude = response.data[0].lon;
+    console.log('success in findLatitudeAndLongitude!', latitude, longitude);
+})
+.catch( (error) => {
+    console.log('error in findLatitudeAndLongitude!');
+});
 
+return {
+    seattleLat: latitude,
+    seattleLon: longitude
+}
+}
 
+const findLocation = (latitude, longitude) => {
+axios.get('https://us1.locationiq.com/v1/reverse.php',
+{
+    params: {
+    key: LOCATIONIQ_KEY,
+    format: 'json',
+    lat: latitude,
+    lon: longitude
+    }
+})
+.then( (response) => {
+    console.log('success in findLocation!', response.data);
+    return response.data;
+})
+.catch( (error) => {
+    console.log('error in findLocation!');
+});
+}
+
+const seattleCoordinates = findLatitudeAndLongitude('Seattle, Washington, USA');
+
+const locations = findLocation(seattleCoordinates.seattleLat, seattleCoordinates.seattleLon);
+
+console.log(locations);
 
 
 
