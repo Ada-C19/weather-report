8f065f722b8f1b9c54a7ef11591bc8c8f1ed3104
Stacey <stacey.l.joseph@gmail.com>
2023-06-13 14:06:15 -0400

    morning updates

--- src/index.js
+++ src/index.js
@@ -37,6 +37,36 @@ const onLoad = () => {
 //     });
 
 
+// additional weather data:
+// let city = 'london'
+// $.ajax({
+//     method: 'GET',
+//     url: 'https://api.api-ninjas.com/v1/weather?city=' + city,
+//     headers: { 'X-Api-Key': 'YOUR_API_KEY'},
+//     contentType: 'application/json',
+//     success: function(result) {
+//         console.log(result);
+//     },
+//     error: function ajaxError(jqXHR) {
+//         console.error('Error: ', jqXHR.responseText);
+//     }
+// });
+
+// random image
+
+let data = 'https://api-ninjas.com';
+let category = 'weather'
+$.ajax({
+    method: 'GET',
+    url: 'https://api.api-ninjas.com/v1/randomimage?category=weather' + category,
+    headers: { 'X-Api-Key': 'API_KEY', 'Accept': 'image/jpg'},
+    success: function(result) {
+        console.log(result);
+    },
+    error: function ajaxError(jqXHR) {
+        console.error('Error: ', jqXHR.responseText);
+    }
+});
 
 
 let citytemp = 72
