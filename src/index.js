e80ed246d4197187a231fa89eba32577a3c2c7cd
Jessica <Yiskah.Scientia@gmail.com>
2023-07-16 20:21:57 -0400

    wave 4 incomplete, api calling weather, not displaying yet

--- src/index.js
+++ src/index.js
@@ -1,104 +1,177 @@
-let temperature = 60;
+// Wave 2
+const state = {
+	temp: 72,
+	city: "Cleveland",
+	coordinates: {
+		lat: 41.4993,
+		long: -81.6944,
+	},
+};
 
 // Wave 2
-const tempValue = document.getElementById('tempValue');
-const increaseTempControl = document.getElementById('increaseTempControl');
-const decreaseTempControl = document.getElementById('decreaseTempControl');
-const landscape = document.getElementById('landscape');
+let temperature = state.temp; 
+const tempValue = document.getElementById("tempValue");
+const increaseTempControl = document.getElementById("increaseTempControl");
+const decreaseTempControl = document.getElementById("decreaseTempControl");
+const landscape = document.getElementById("landscape");
 
 function updateTemperature() {
-    tempValue.textContent = temperature;
+	tempValue.textContent = temperature;
 
-    if (temperature >= 80) {
-        tempValue.style.color = 'red';
-    } else if (temperature >= 70 && temperature <= 79) {
-        tempValue.style.color = 'orange';
-    } else if (temperature >= 60 && temperature <= 69) {
-        tempValue.style.color = 'yellow';
-    } else if (temperature >= 50 && temperature <= 59) {
-        tempValue.style.color = 'green';
-    } else {
-        tempValue.style.color = 'blue'; // teal showed up as too similar on my screen and made testing hard
-    }
-    }
+	if (temperature >= 80) {
+		tempValue.style.color = "red";
+	} else if (temperature >= 70 && temperature <= 79) {
+		tempValue.style.color = "orange";
+	} else if (temperature >= 60 && temperature <= 69) {
+		tempValue.style.color = "yellow";
+	} else if (temperature >= 50 && temperature <= 59) {
+		tempValue.style.color = "green";
+	} else {
+		tempValue.style.color = "blue";
+	}
+}
 
 function updateLandscape() {
-    if (temperature >= 80) {
-    landscape.textContent = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂";
-    } else if (temperature >= 70 && temperature <= 79) {
-    landscape.textContent = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
-    } else if (temperature >= 60 && temperature <= 69) {
-    landscape.textContent = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
-    } else {
-    landscape.textContent = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
-    }
+	if (temperature >= 80) {
+		landscape.textContent = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂";
+	} else if (temperature >= 70 && temperature <= 79) {
+		landscape.textContent = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
+	} else if (temperature >= 60 && temperature <= 69) {
+		landscape.textContent = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
+	} else {
+		landscape.textContent = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+	}
 }
 
 function increaseTemperature() {
-    temperature++;
-    updateTemperature();
-    updateLandscape();
+	temperature++;
+	updateTemperature();
+	updateLandscape();
 }
 
 function decreaseTemperature() {
-    temperature--;
-    updateTemperature();
-    updateLandscape();
+	temperature--;
+	updateTemperature();
+	updateLandscape();
 }
 
 // Wave 3
-const headerCityName = document.getElementById('headerCityName');
-const cityNameInput = document.getElementById('cityNameInput');
+const headerCityName = document.getElementById("headerCityName");
+const cityNameInput = document.getElementById("cityNameInput");
 
-function updateCityName() {
-    headerCityName.textContent = cityNameInput.value;
+function updateCityName(event) {
+	if (event.key === "Enter") {
+		state.city = cityNameInput.value;
+		headerCityName.textContent = state.city;
+		findCoordinates();
+	}
 }
 
-// Wave 5
-const skySelect = document.getElementById('skySelect');
-const sky = document.getElementById('sky');
+cityNameInput.addEventListener("keydown", updateCityName); 
+
+// Wave 4
+const findCoordinates = () => {
+	if (state.city.length < 3) {
+		return; 
+	}
+
+	axios
+		.get("http://127.0.0.1:5000/location", {
+			params: {
+				q: state.city,
+			},
+		})
+		.then((response) => {
+			state.coordinates.lat = response.data[0].lat;
+			state.coordinates.long = response.data[0].lon;
+			updateCoordinates();
+			getWeather(); 
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+};
+
+function updateCoordinates() {
+	const coordinatesElement = document.getElementById("coordinates");
+	coordinatesElement.textContent = `Latitude: ${state.coordinates.lat}, Longitude: ${state.coordinates.long}`;
+}
+
+const getWeather = () => {
+	axios
+		.get("http://127.0.0.1:5000/weather", {
+			params: {
+				lat: state.coordinates.lat,
+				lon: state.coordinates.long,
+			},
+		})
+		.then((response) => {
+			const weatherData = response.data;
+            console.log(weatherData); 
+			const weatherElement = document.getElementById("weather");
+			weatherElement.textContent = `Temperature: ${weatherData.main.temp}°C, Weather: ${weatherData.weather[0].description}`;
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+};
+
+const skySelect = document.getElementById("skySelect");
+const sky = document.getElementById("sky");
 
 function updateSky() {
-    const selectedSky = skySelect.value;
+	const selectedSky = skySelect.value;
 
+	sky.textContent = "";
 
-    sky.textContent = '';
-
-    switch (selectedSky) {
-        case 'sunny':
-            sky.textContent = '☁️ ☁️ ☁️ ☀️ ☁️ ☁️';
-            break;
-        case 'cloudy':
-            sky.textContent = '☁️☁️ ☁️ ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️';
-            break;
-        case 'rainy':
-            sky.textContent = '🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧🌧';
-            break;
-        case 'snowy':
-            sky.textContent = '🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨';
-            break;
-        default:
-            break;
-        }
-    }
-
-// Wave 6
-const cityNameReset = document.getElementById('cityNameReset');
-const defaultCityName = 'Cleveland';
-
-function resetCityName() {
-    cityNameInput.value = defaultCityName;
-    updateCityName();
+	switch (selectedSky) {
+		case "sunny":
+			sky.textContent = "☁️ ☁️ ☁️ ☀️ ☁️ ☁️";
+			break;
+		case "cloudy":
+			sky.textContent = "☁️☁️ ☁️ ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️";
+			break;
+		case "rainy":
+			sky.textContent = "🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧🌧";
+			break;
+		case "snowy":
+			sky.textContent = "🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨";
+			break;
+		default:
+			break;
+	}
 }
 
-// Event
-increaseTempControl.addEventListener('click', increaseTemperature);
-decreaseTempControl.addEventListener('click', decreaseTemperature);
-cityNameInput.addEventListener('input', updateCityName);
-cityNameReset.addEventListener('click', resetCityName);
-skySelect.addEventListener('change', updateSky);
+skySelect.value = "sunny";
+updateSky();
+
+const cityNameReset = document.getElementById("cityNameReset");
+const defaultCityName = "Cleveland";
+
+function resetCityName() {
+	state.city = defaultCityName;
+	cityNameInput.value = state.city;
+	headerCityName.textContent = state.city;
+	findCoordinates();
+}
+
+cityNameInput.value = state.city;
+headerCityName.textContent = state.city;
+
+// Event Listeners
+increaseTempControl.addEventListener("click", increaseTemperature);
+decreaseTempControl.addEventListener("click", decreaseTemperature);
+cityNameReset.addEventListener("click", resetCityName);
+skySelect.addEventListener("change", updateSky);
 
 // Initializations
 updateTemperature();
 updateLandscape();
-updateCityName();
+updateCoordinates();
+
+
+
+
+
+
+
