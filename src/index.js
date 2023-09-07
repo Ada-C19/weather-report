426e39eeee1f75ec58301d754e8abd68ddf7a130
Luwam Ghebremicael <luwammikael@gmail.com>
2023-09-07 15:22:17 -0700

    adds render link

--- src/index.js
+++ src/index.js
@@ -132,7 +132,8 @@ selectedSkyCondition.addEventListener("click", function () {
   updateSky();
 });
 
-const base_url = process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:5000"
+// const base_url = process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:5000"
+const base_url = "https://weather-report-server-j4vl.onrender.com"
 const getLatLon = () => {
   axios
     .get(`${base_url}/location`, {
